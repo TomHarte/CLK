8c1bfa5a0589503d23e63058735a0735bccaee62
Thomas Harte <thomas.harte@gmail.com>
2016-01-07 20:36:27 -0500

    This is the bare minimum to prove that the ROM is trying properly to boot.

--- Machines/Electron/Electron.cpp
+++ Machines/Electron/Electron.cpp
@@ -23,8 +23,6 @@ Machine::~Machine()
 
 unsigned int Machine::perform_bus_operation(CPU6502::BusOperation operation, uint16_t address, uint8_t *value)
 {
-	printf("%04x\n", address);
-
 	if(address < 32768)
 	{
 		if(isReadOperation(operation))
@@ -40,7 +38,43 @@ unsigned int Machine::perform_bus_operation(CPU6502::BusOperation operation, uin
 	{
 		if(address > 49152)
 		{
-			if(isReadOperation(operation)) *value = os[address - 49152];
+			if((address & 0xff00) == 0xfe00)
+			{
+				printf("%c: %02x: ", isReadOperation(operation) ? 'r' : 'w', *value);
+
+				switch(address&0xf)
+				{
+					case 0x0:
+						printf("Interrupt status or control\n");
+					break;
+					case 0x1:
+					break;
+					case 0x2:
+					case 0x3:
+						printf("Screen start address\n");
+					break;
+					case 0x4:
+						printf("Cassette\n");
+					break;
+					case 0x5:
+						printf("Interrupt clear and paging\n");
+					break;
+					case 0x6:
+						printf("Counter\n");
+					break;
+					case 0x7:
+						printf("Misc. control\n");
+					break;
+					default:
+						printf("Palette\n");
+					break;
+				}
+			}
+			else
+			{
+				if(isReadOperation(operation))
+					*value = os[address - 49152];
+			}
 		}
 		else
 		{
