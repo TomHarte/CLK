069ec2e8891b93c032653691dfaa5cf858431765
Thomas Harte <thomas.harte@gmail.com>
2016-04-11 23:13:54 -0400

    In search of the cause of performance issues again, excised the per-pixel sin until it can be further evaluated.

--- Outputs/CRT/Internals/CRTOpenGL.cpp
+++ Outputs/CRT/Internals/CRTOpenGL.cpp
@@ -417,7 +417,7 @@ char *OpenGLOutputBuilder::get_output_fragment_shader(const char *sampling_funct
 
 		"void main(void)"
 		"{"
-			"fragColour = rgb_sample(texID, srcCoordinatesVarying, iSrcCoordinatesVarying) * vec4(1.0, 1.0, 1.0, alpha*sin(lateralVarying));" //
+			"fragColour = rgb_sample(texID, srcCoordinatesVarying, iSrcCoordinatesVarying) * vec4(1.0, 1.0, 1.0, alpha);" //*sin(lateralVarying)
 		"}"
 	, sampling_function);
 }
