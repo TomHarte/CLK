81518ae774925ed444107ce4bba04492ef190f77
Thomas Harte <thomas.harte@gmail.com>
2016-06-19 08:41:27 -0400

    Albeit with great ugliness, this at least gets as far as having the BASIC images on screen.

--- OSBindings/Mac/Clock Signal/Document Controller/NewDocumentViewController.swift
+++ OSBindings/Mac/Clock Signal/Document Controller/NewDocumentViewController.swift
@@ -9,14 +9,55 @@
 import Cocoa
 
 class NewDocumentCollectionViewItem: NSCollectionViewItem {
+	override var representedObject: AnyObject? {
+		didSet {
+			if let filename = representedObject as? String, imageView = imageView {
+				imageView.image = NSImage(named: filename)
+			}
+		}
+	}
+
+	override var selected: Bool {
+		didSet {
+			var colour: NSColor!
+			var lineColour: NSColor!
+ 
+			if selected {
+				colour		= NSColor.selectedControlColor()
+				lineColour	= NSColor.blackColor()
+			} else {
+				colour		= NSColor.controlBackgroundColor()
+				lineColour	= NSColor.controlBackgroundColor()
+			}
+
+			if let box = self.view as? NSBox {
+				box.borderColor = lineColour
+				box.fillColor = colour
+			}
+		}
+	}
 }
 
 class NewDocumentViewController: NSViewController {
 
 	@IBOutlet var collectionView : NSCollectionView!
+	var collectionViewItems : [String] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-		collectionView.maxNumberOfRows = 1
+		collectionView.minItemSize = NSSize(width: 200, height: 150)
+
+		let itemCopy: NewDocumentCollectionViewItem! = NewDocumentCollectionViewItem().copyWithZone(nil) as! NewDocumentCollectionViewItem
+		collectionView.itemPrototype = itemCopy
+		collectionView.content = ["ElectronBASIC", "Vic20BASIC"]
     }
+
+	@IBAction func cancel(sender : AnyObject!) {
+		self.view.window?.close()
+	}
+
+	@IBAction func choose(sender : AnyObject!) {
+		print("\(collectionView.selectionIndexes)")
+	}
 }
