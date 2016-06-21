25a5455d33f28e6582695d5aa270ab7f08fc6e54
Thomas Harte <thomas.harte@gmail.com>
2016-06-20 21:07:01 -0400

    Completed bridge interface.

--- OSBindings/Mac/Clock SignalTests/MOS6532Bridge.mm
+++ OSBindings/Mac/Clock SignalTests/MOS6532Bridge.mm
@@ -10,6 +10,12 @@
 #include "6532.hpp"
 
 class VanillaRIOT: public MOS::MOS6532<VanillaRIOT> {
+	public:
+		uint8_t get_port_input(int port)
+		{
+			return input[port];
+		}
+		uint8_t input[2];
 };
 
 @implementation MOS6532Bridge
@@ -35,6 +41,16 @@ class VanillaRIOT: public MOS::MOS6532<VanillaRIOT> {
 - (BOOL)irqLine
 {
 	return _riot.get_inerrupt_line();
+}
+
+- (void)setPortAInput:(uint8_t)portAInput
+{
+	_riot.input[0] = _portAInput = portAInput;
+}
+
+- (void)setPortBInput:(uint8_t)portBInput
+{
+	_riot.input[1] = _portBInput = portBInput;
 }
 
 @end
