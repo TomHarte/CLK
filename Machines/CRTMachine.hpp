ed8f4d0476988f3cfa553b49849825b37976aba0
Thomas Harte <thomasharte@google.com>
2016-06-30 08:46:29 -0400

    Added some minor additional bits of documentation.

--- Machines/CRTMachine.hpp
+++ Machines/CRTMachine.hpp
@@ -14,6 +14,11 @@
 
 namespace CRTMachine {
 
+/*!
+	A CRTMachine::Machine is a mostly-abstract base class for machines that connect to a CRT,
+	that optionally provide a speaker, and that nominate a clock rate and can announce to a delegate
+	should that clock rate change.
+*/
 class Machine {
 	public:
 		virtual void setup_output(float aspect_ratio) = 0;
@@ -24,6 +29,7 @@ class Machine {
 
 		virtual void run_for_cycles(int number_of_cycles) = 0;
 
+		// TODO: sever the clock-rate stuff.
 		virtual double get_clock_rate() = 0;
 		class Delegate {
 			public:
