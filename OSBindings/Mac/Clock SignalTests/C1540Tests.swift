865eb421cd1a8f612f748da14a4afe2d63d2aa15
Thomas Harte <thomas.harte@gmail.com>
2016-07-09 15:44:55 -0400

    Quick on-disk tidy up.

---
+++ OSBindings/Mac/Clock SignalTests/C1540Tests.swift
@@ -0,0 +1,13 @@
+//
+//  C1540Tests.swift
+//  Clock Signal
+//
+//  Created by Thomas Harte on 09/07/2016.
+//  Copyright © 2016 Thomas Harte. All rights reserved.
+//
+
+import XCTest
+
+class C1540Tests: XCTestCase {
+
+}
