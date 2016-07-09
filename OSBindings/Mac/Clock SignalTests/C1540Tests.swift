da6fe2e9832501672afc62d1dcde96fa79d424f9
Thomas Harte <thomas.harte@gmail.com>
2016-07-09 15:47:53 -0400

    This should be enough of a shell to write some actual tests.

--- OSBindings/Mac/Clock SignalTests/C1540Tests.swift
+++ OSBindings/Mac/Clock SignalTests/C1540Tests.swift
@@ -10,4 +10,22 @@ import XCTest
 
 class C1540Tests: XCTestCase {
 
+	private func with1540(action: (C1540Bridge) -> ()) {
+		let bridge = C1540Bridge()
+
+		if let path = NSBundle.mainBundle().pathForResource("1541", ofType: "bin", inDirectory: "ROMImages/Commodore1540") {
+			let data = NSData(contentsOfFile: path)
+			bridge.setROM(data)
+		}
+
+		action(bridge)
+	}
+
+	// MARK: EOI
+
+	func testEOI() {
+		with1540 {
+			$0.runForCycles(20)
+		}
+	}
 }
