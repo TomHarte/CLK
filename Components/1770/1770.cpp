e3daf805644124f2b7cc6f66477c923973228207
Thomas Harte <thomas.harte@gmail.com>
2016-09-17 18:01:00 -0400

    Added a file for the 1770, at least.

---
+++ Components/1770/1770.cpp
@@ -0,0 +1,9 @@
+//
+//  1770.cpp
+//  Clock Signal
+//
+//  Created by Thomas Harte on 17/09/2016.
+//  Copyright © 2016 Thomas Harte. All rights reserved.
+//
+
+#include "1770.hpp"
---
+++ Components/1770/1770.hpp
@@ -0,0 +1,14 @@
+//
+//  1770.hpp
+//  Clock Signal
+//
+//  Created by Thomas Harte on 17/09/2016.
+//  Copyright © 2016 Thomas Harte. All rights reserved.
+//
+
+#ifndef _770_hpp
+#define _770_hpp
+
+#include <stdio.h>
+
+#endif /* _770_hpp */
