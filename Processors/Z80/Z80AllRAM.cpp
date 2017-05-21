08206eea56e7749c7a74382e0cb54f70102e1f6b
Thomas Harte <thomas.harte@gmail.com>
2017-05-21 09:47:53 -0400

    This logging has outlived its usefulness for now.

--- Processors/Z80/Z80AllRAM.cpp
+++ Processors/Z80/Z80AllRAM.cpp
@@ -16,14 +16,14 @@ AllRAMProcessor::AllRAMProcessor() : ::CPU::AllRAMProcessor(65536) {}
 int AllRAMProcessor::perform_machine_cycle(const MachineCycle *cycle) {
 	switch(cycle->operation) {
 		case BusOperation::ReadOpcode:
-			printf("! ");
+//			printf("! ");
 			check_address_for_trap(*cycle->address);
 		case BusOperation::Read:
-			printf("r %04x [%02x] AF:%04x BC:%04x DE:%04x HL:%04x SP:%04x\n", *cycle->address, memory_[*cycle->address], get_value_of_register(CPU::Z80::Register::AF), get_value_of_register(CPU::Z80::Register::BC), get_value_of_register(CPU::Z80::Register::DE), get_value_of_register(CPU::Z80::Register::HL), get_value_of_register(CPU::Z80::Register::StackPointer));
+//			printf("r %04x [%02x] AF:%04x BC:%04x DE:%04x HL:%04x SP:%04x\n", *cycle->address, memory_[*cycle->address], get_value_of_register(CPU::Z80::Register::AF), get_value_of_register(CPU::Z80::Register::BC), get_value_of_register(CPU::Z80::Register::DE), get_value_of_register(CPU::Z80::Register::HL), get_value_of_register(CPU::Z80::Register::StackPointer));
 			*cycle->value = memory_[*cycle->address];
 		break;
 		case BusOperation::Write:
-			printf("w %04x\n", *cycle->address);
+//			printf("w %04x\n", *cycle->address);
 			memory_[*cycle->address] = *cycle->value;
 		break;
 
