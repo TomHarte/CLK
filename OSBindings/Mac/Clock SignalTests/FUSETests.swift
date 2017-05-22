e3e461d7cbb340604ed27aa31f82f7b86cba38b8
Thomas Harte <thomas.harte@gmail.com>
2017-05-21 22:49:24 -0400

    Added a test class for running the FUSE tests. With nothing much in it.

---
+++ OSBindings/Mac/Clock SignalTests/FUSETests.swift
@@ -0,0 +1,30 @@
+//
+//  FUSETests.swift
+//  Clock Signal
+//
+//  Created by Thomas Harte on 21/05/2017.
+//  Copyright © 2017 Thomas Harte. All rights reserved.
+//
+
+import XCTest
+import Foundation
+
+class FUSETests: XCTestCase {
+
+	func testFUSE() {
+		if	let inputFilename = Bundle(for: type(of: self)).path(forResource: "tests", ofType: "in"),
+			let outputFilename = Bundle(for: type(of: self)).path(forResource: "tests", ofType: "expected") {
+			if	let input = try? String(contentsOf: URL(fileURLWithPath: inputFilename)),
+				let output = try? String(contentsOf: URL(fileURLWithPath: outputFilename)) {
+
+				let machine = CSTestMachineZ80()
+//				machine.setData(testData, atAddress: 0x0100)
+
+//				machine.setValue(0x0100, for: .programCounter)
+
+//				machine.runForNumber(ofCycles: 20)
+			}
+		}
+	}
+
+}
