350b36df2550f1c8c6ab63f0c3c0b367af206d68
Thomas Harte <thomas.harte@gmail.com>
2017-06-07 17:30:53 -0400

    Wired in an attempted usage of the new tape parser.

--- StaticAnalyser/ZX8081/StaticAnalyser.cpp
+++ StaticAnalyser/ZX8081/StaticAnalyser.cpp
@@ -8,14 +8,39 @@
 
 #include "StaticAnalyser.hpp"
 
+#include <string>
+#include <vector>
+
+#include "../../Storage/Tape/Parsers/ZX8081.hpp"
+
+static std::vector<Storage::Tape::ZX8081::File> GetFiles(const std::shared_ptr<Storage::Tape::Tape> &tape) {
+	std::vector<Storage::Tape::ZX8081::File> files;
+	Storage::Tape::ZX8081::Parser parser;
+
+	while(!tape->is_at_end()) {
+		std::shared_ptr<Storage::Tape::ZX8081::File> next_file = parser.get_next_file(tape);
+		if(next_file != nullptr) {
+			files.push_back(*next_file);
+		}
+	}
+
+	return files;
+}
+
 void StaticAnalyser::ZX8081::AddTargets(
 		const std::list<std::shared_ptr<Storage::Disk::Disk>> &disks,
 		const std::list<std::shared_ptr<Storage::Tape::Tape>> &tapes,
 		const std::list<std::shared_ptr<Storage::Cartridge::Cartridge>> &cartridges,
 		std::list<StaticAnalyser::Target> &destination) {
-	// Temporary: be entirely trusting.
-	StaticAnalyser::Target target;
-	target.machine = Target::ZX80;
-	target.tapes = tapes;
-	destination.push_back(target);
+
+	if(!tapes.empty()) {
+		std::vector<Storage::Tape::ZX8081::File> files = GetFiles(tapes.front());
+		if(!files.empty()) {
+			// TODO: check files for machine type, memory size.
+			StaticAnalyser::Target target;
+			target.machine = Target::ZX80;
+			target.tapes = tapes;
+			destination.push_back(target);
+		}
+	}
 }
