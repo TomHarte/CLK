23e989e170af67a0e282f9787fd057f75476584e
Thomas Harte <thomas.harte@gmail.com>
2017-07-08 19:12:06 -0400

    This will likely do for the Swift/XIB side of things: the play/pause button is enabled or disabled as per the user's choice of automatic tape control, and toggles function when pressed. It communicates activity down to the Objective-C[++] layer, giving it a route through to the actual machine.

--- OSBindings/Mac/Clock Signal/Documents/ZX8081OptionsPanel.swift
+++ OSBindings/Mac/Clock Signal/Documents/ZX8081OptionsPanel.swift
@@ -6,15 +6,43 @@
 //  Copyright © 2017 Thomas Harte. All rights reserved.
 //
 
-import Cocoa
-
 class ZX8081OptionsPanel: MachinePanel {
-
-	@IBOutlet var tapeControlButton: NSButton!
-	@IBAction func setAutomaticTapeConrol(_ sender: NSButton!) {
-		Swift.print("tape control \(tapeControlButton)")
+	var zx8081: CSZX8081! {
+		get {
+			return self.machine as! CSZX8081
+		}
 	}
+
+	@IBOutlet var automaticTapeMotorControlButton: NSButton!
+	var automaticTapeMotorControlDefaultsKey: String {
+		get { return prefixedUserDefaultsKey("automaticTapeMotorControl") }
+	}
+	@IBAction func setAutomaticTapeMotorConrol(_ sender: NSButton!) {
+		let isEnabled = sender.state == NSOnState
+		UserDefaults.standard.set(isEnabled, forKey: self.automaticTapeMotorControlDefaultsKey)
+		self.playOrPauseTapeButton.isEnabled = !isEnabled
+		self.zx8081.useAutomaticTapeMotorControl = isEnabled
+	}
+
+	@IBOutlet var playOrPauseTapeButton: NSButton!
 	@IBAction func playOrPauseTape(_ sender: NSButton!) {
-		Swift.print("play/pause")
+		self.zx8081.tapeIsPlaying = !self.zx8081.tapeIsPlaying
+		self.playOrPauseTapeButton.title = self.zx8081.tapeIsPlaying
+			? NSLocalizedString("Stop Tape", comment: "Text for a button that will stop tape playback")
+			: NSLocalizedString("Play Tape", comment: "Text for a button that will start tape playback")
+	}
+
+	// MARK: option restoration
+	override func establishStoredOptions() {
+		super.establishStoredOptions()
+
+		let standardUserDefaults = UserDefaults.standard
+		standardUserDefaults.register(defaults: [
+			self.automaticTapeMotorControlDefaultsKey: true
+		])
+
+		let automaticTapeMotorControlIsEnabled = standardUserDefaults.bool(forKey: self.automaticTapeMotorControlDefaultsKey)
+		self.automaticTapeMotorControlButton.state = automaticTapeMotorControlIsEnabled ? NSOnState : NSOffState
+		self.playOrPauseTapeButton.isEnabled = !automaticTapeMotorControlIsEnabled
 	}
 }
