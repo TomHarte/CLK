2a9dccff26c751a68bdae50359292372472efc29
Thomas Harte <thomas.harte@gmail.com>
2017-11-13 22:28:11 -0500

    Fixes typo.

--- Outputs/CRT/Internals/ArrayBuilder.cpp
+++ Outputs/CRT/Internals/ArrayBuilder.cpp
@@ -125,7 +125,7 @@ std::size_t ArrayBuilder::Buffer::submit(bool is_input) {
 	} else {
 		glBindBuffer(GL_ARRAY_BUFFER, buffer);
 		uint8_t *destination = static_cast<uint8_t *>(glMapBufferRange(GL_ARRAY_BUFFER, 0, (GLsizeiptr)length, GL_MAP_WRITE_BIT | GL_MAP_UNSYNCHRONIZED_BIT | GL_MAP_FLUSH_EXPLICIT_BIT));
-		if(false) {
+		if(destination) {
 			std::memcpy(destination, data.data(), length);
 			glFlushMappedBufferRange(GL_ARRAY_BUFFER, 0, (GLsizeiptr)length);
 			glUnmapBuffer(GL_ARRAY_BUFFER);
