576d554a2c17d798c4cf14c97d2ac55c010e8266
Thomas Harte <thomas.harte@gmail.com>
2017-11-25 13:33:51 -0500

    Expands upon the MSX skeleton.

--- Machines/MSX/MSX.cpp
+++ Machines/MSX/MSX.cpp
@@ -14,14 +14,37 @@
 #include "../../Components/8255/i8255.hpp"
 #include "../../Components/AY38910/AY38910.hpp"
 
+#include "../CRTMachine.hpp"
+
 namespace MSX {
 
 class ConcreteMachine:
+	public Machine,
 	public CPU::Z80::BusHandler,
-	public Machine {
+	public CRTMachine::Machine {
 	public:
 		ConcreteMachine():
-			z80_(*this) {}
+			z80_(*this) {
+			set_clock_rate(3579545);
+		}
+
+		void setup_output(float aspect_ratio) override {
+		}
+
+		void close_output() override {
+		}
+
+		std::shared_ptr<Outputs::CRT::CRT> get_crt() override {
+			return nullptr;
+		}
+
+		std::shared_ptr<Outputs::Speaker> get_speaker() override {
+			return nullptr;
+		}
+
+		void run_for(const Cycles cycles) override {
+			z80_.run_for(cycles);
+		}
 
 	private:
 		CPU::Z80::Processor<ConcreteMachine, false, false> z80_;
