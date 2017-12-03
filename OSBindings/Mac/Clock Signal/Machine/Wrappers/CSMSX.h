ee44d671e74bca64f1bb72e44839bad79b8d28bf
Thomas Harte <thomas.harte@gmail.com>
2017-12-03 15:42:54 -0500

    Steps towards exposing the MSX in Cocoa builds.

---
+++ OSBindings/Mac/Clock Signal/Machine/Wrappers/CSMSX.h
@@ -0,0 +1,15 @@
+//
+//  CSMSX.h
+//  Clock Signal
+//
+//  Created by Thomas Harte on 03/12/2017.
+//  Copyright © 2017 Thomas Harte. All rights reserved.
+//
+
+#import "CSMachine.h"
+
+@interface CSMSX : CSMachine
+
+- (instancetype)init;
+
+@end
