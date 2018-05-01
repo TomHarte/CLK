9da481b060da9be29ffe15b2b9958647081229c9
Thomas Harte <thomas.harte@gmail.com>
2018-04-30 22:08:51 -0400

    Slightly simplifies syntax.

--- Storage/Disk/DiskImage/Formats/HFE.cpp
+++ Storage/Disk/DiskImage/Formats/HFE.cpp
@@ -79,9 +79,7 @@ std::shared_ptr<Track> HFE::get_track_at_position(Track::Address address) {
 	// Flip bytes; HFE's preference is that the least-significant bit
 	// is serialised first, but PCMTrack posts the most-significant first.
 	Storage::Data::BitReverse::reverse(segment.data);
-
-	std::shared_ptr<Track> track(new PCMTrack(segment));
-	return track;
+	return std::make_shared<PCMTrack>(segment);
 }
 
 void HFE::set_tracks(const std::map<Track::Address, std::shared_ptr<Track>> &tracks) {
