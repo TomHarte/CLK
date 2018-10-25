8730ffb4e24992a0d4061aa3d924cb9bba93c292
Thomas Harte <thomas.harte@gmail.com>
2018-10-24 22:20:58 -0400

    Restores multi-machine keyboard propagation.

--- Analyser/Dynamic/MultiMachine/Implementation/MultiKeyboardMachine.hpp
+++ Analyser/Dynamic/MultiMachine/Implementation/MultiKeyboardMachine.hpp
@@ -25,6 +25,25 @@ namespace Dynamic {
 	order of delivered messages.
 */
 class MultiKeyboardMachine: public KeyboardMachine::Machine {
+	private:
+		std::vector<::KeyboardMachine::Machine *> machines_;
+
+		class MultiKeyboard: public Inputs::Keyboard {
+			public:
+				MultiKeyboard(const std::vector<::KeyboardMachine::Machine *> &machines);
+
+				void set_key_pressed(Key key, char value, bool is_pressed) override;
+				void reset_all_keys() override;
+				const std::set<Key> &observed_keys() override;
+				bool is_exclusive() override;
+
+			private:
+				const std::vector<::KeyboardMachine::Machine *> &machines_;
+				std::set<Key> observed_keys_;
+				bool is_exclusive_ = false;
+		};
+		MultiKeyboard keyboard_;
+
 	public:
 		MultiKeyboardMachine(const std::vector<std::unique_ptr<::Machine::DynamicMachine>> &machines);
 
@@ -33,10 +52,6 @@ class MultiKeyboardMachine: public KeyboardMachine::Machine {
 		void set_key_state(uint16_t key, bool is_pressed) override;
 		void type_string(const std::string &) override;
 		Inputs::Keyboard &get_keyboard() override;
-
-	private:
-		std::vector<::KeyboardMachine::Machine *> machines_;
-		Inputs::Keyboard keyboard_;
 };
 
 }
