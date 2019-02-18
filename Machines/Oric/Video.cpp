40ee215b1b2c35f253c4b36c03ee563a9c9f02aa
Thomas Harte <thomas.harte@gmail.com>
2019-02-18 17:20:52 -0500

    By #define provides a means not to use the real composite samples.
    To aid with debugging.

--- Machines/Oric/Video.cpp
+++ Machines/Oric/Video.cpp
@@ -10,6 +10,8 @@
 
 #include <algorithm>
 
+#define SUPPLY_COMPOSITE
+
 using namespace Oric;
 
 namespace {
@@ -29,16 +31,23 @@ VideoOutput::VideoOutput(uint8_t *memory) :
 	crt_.set_visible_area(crt_.get_rect_for_area(54, 224, 16 * 6, 40 * 6, 4.0f / 3.0f));
 	crt_.set_phase_linked_luminance_offset(-1.0f / 8.0f);
 	data_type_ = Outputs::Display::InputDataType::Red1Green1Blue1;
+	crt_.set_input_data_type(data_type_);
 }
 
 void VideoOutput::set_display_type(Outputs::Display::DisplayType display_type) {
+	crt_.set_display_type(display_type);
+
+#ifdef SUPPLY_COMPOSITE
 	const auto data_type =
 		(display_type == Outputs::Display::DisplayType::RGB) ?
 			Outputs::Display::InputDataType::Red1Green1Blue1 :
 			Outputs::Display::InputDataType::PhaseLinkedLuminance8;
+#else
+	const auto data_type = Outputs::Display::InputDataType::Red1Green1Blue1;
+#endif
+
 	if(data_type_ != data_type) {
 		data_type_ = data_type;
-		crt_.set_display_type(display_type);
 		crt_.set_input_data_type(data_type_);
 	}
 }
