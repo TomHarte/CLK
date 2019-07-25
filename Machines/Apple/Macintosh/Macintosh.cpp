109d5d16bd451546e3d7835bf74d3328beb363d3
Thomas Harte <thomas.harte@gmail.com>
2019-07-25 10:33:38 -0400

    Withdraws optimisation, for further testing in the future.

--- Machines/Apple/Macintosh/Macintosh.cpp
+++ Machines/Apple/Macintosh/Macintosh.cpp
@@ -319,7 +319,7 @@ template <Analyser::Static::Macintosh::Target::Model model> class ConcreteMachin
 				// It embodies knowledge of the fact that video (and audio) will always
 				// be fetched from the final $d900 bytes (i.e. $6c80 words) of memory.
 				// (And that ram_mask_ = ram size - 1).
-				if(word_address > ram_mask_ - 0x6c80)
+//				if(word_address > ram_mask_ - 0x6c80)
 					update_video();
 			} else {
 				memory_base = rom_;
