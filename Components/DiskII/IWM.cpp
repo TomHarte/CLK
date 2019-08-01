9bbccd89d31c389922333effce21de87b01c6052
Thomas Harte <thomas.harte@gmail.com>
2019-07-31 23:19:46 -0400

    Adds an extended rationale for current implementation.
    Also strips some cruft of prior guesses.

--- Components/DiskII/IWM.cpp
+++ Components/DiskII/IWM.cpp
@@ -63,6 +63,7 @@ uint8_t IWM::read(int address) {
 
 			if(data_register_ & 0x80) {
 //				printf("\n\nIWM:%02x\n\n", data_register_);
+//				printf(".");
 				data_register_ = 0;
 			}
 //			LOG("Reading data register: " << PADHEX(2) << int(result));
