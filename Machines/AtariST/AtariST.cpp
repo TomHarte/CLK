125f781ced1841b14851983350ecf4e4d6a8dbec
Thomas Harte <thomas.harte@gmail.com>
2019-10-03 22:47:57 -0400

    Starts to create an actual shell of a machine.

--- Machines/AtariST/AtariST.cpp
+++ Machines/AtariST/AtariST.cpp
@@ -8,10 +8,39 @@
 
 #include "AtariST.hpp"
 
+#include "../CRTMachine.hpp"
+
+namespace {
+
+const int CLOCK_RATE = 8000000;
+
+using Target = Analyser::Static::Target;
+
+class ConcreteMachine:
+	public Atari::ST::Machine,
+	public CRTMachine::Machine {
+	public:
+		ConcreteMachine(const Target &target, const ROMMachine::ROMFetcher &rom_fetcher) {
+			set_clock_rate(CLOCK_RATE);
+		}
+
+		void set_scan_target(Outputs::Display::ScanTarget *scan_target) final {
+		}
+
+		Outputs::Speaker::Speaker *get_speaker() final {
+			return nullptr;
+		}
+
+		void run_for(const Cycles cycles) final {
+		}
+};
+
+}
+
 using namespace Atari::ST;
 
 Machine *Machine::AtariST(const Analyser::Static::Target *target, const ROMMachine::ROMFetcher &rom_fetcher) {
-	return nullptr;
+	return new ConcreteMachine(*target, rom_fetcher);
 }
 
 Machine::~Machine() {}
