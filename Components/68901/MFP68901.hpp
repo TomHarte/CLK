885f890df1af07a3d8f805343f84ccb918c343e0
Thomas Harte <thomas.harte@gmail.com>
2019-10-06 23:14:05 -0400

    Adds a target for MFP read/write operations.
    Completely without any implementation, so far.

---
+++ Components/68901/MFP68901.hpp
@@ -0,0 +1,30 @@
+//
+//  MFP68901.hpp
+//  Clock Signal
+//
+//  Created by Thomas Harte on 06/10/2019.
+//  Copyright © 2019 Thomas Harte. All rights reserved.
+//
+
+#ifndef MFP68901_hpp
+#define MFP68901_hpp
+
+#include <cstdint>
+#include "../../ClockReceiver/ClockReceiver.hpp"
+
+namespace Motorola {
+namespace MFP68901 {
+
+class MFP68901 {
+	public:
+		uint8_t read(int address);
+		void write(int address, uint8_t value);
+
+		void run_for(HalfCycles);
+		HalfCycles get_next_sequence_point();
+};
+
+}
+}
+
+#endif /* MFP68901_hpp */
