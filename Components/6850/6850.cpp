cd75978e4e76cf143b5fc3a3178974e06ffed49f
Thomas Harte <thomas.harte@gmail.com>
2019-10-12 00:04:02 -0400

    Nudges 6850 towards coherence.

--- Components/6850/6850.cpp
+++ Components/6850/6850.cpp
@@ -8,13 +8,31 @@
 
 #include "6850.hpp"
 
+#define LOG_PREFIX "[6850] "
+#include "../../Outputs/Log.hpp"
+
 using namespace Motorola::ACIA;
 
 uint8_t ACIA::read(int address) {
-	return 0xff;
+	if(address&1) {
+		LOG("Read from receive register");
+	} else {
+		LOG("Read status");
+		return status_;
+	}
+	return 0x00;
 }
 
 void ACIA::write(int address, uint8_t value) {
+	if(address&1) {
+		LOG("Write to transmit register");
+	} else {
+		if((value&3) == 3) {
+			LOG("Reset");
+		} else {
+			LOG("Write to control register");
+		}
+	}
 }
 
 void ACIA::run_for(HalfCycles) {
