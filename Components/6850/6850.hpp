4b09d7c41dc17a7b51148b4387a4d2853625793a
Thomas Harte <thomas.harte@gmail.com>
2019-10-12 00:04:02 -0400

    Nudges 6850 towards coherence.

--- Components/6850/6850.hpp
+++ Components/6850/6850.hpp
@@ -17,10 +17,29 @@ namespace ACIA {
 
 class ACIA {
 	public:
+		/*!
+			Reads from the ACIA.
+
+			Bit 0 of the address is used as the ACIA's register select line —
+			so even addresses select control/status registers, odd addresses
+			select transmit/receive data registers.
+		*/
 		uint8_t read(int address);
+
+		/*!
+			Writes to the ACIA.
+
+			Bit 0 of the address is used as the ACIA's register select line —
+			so even addresses select control/status registers, odd addresses
+			select transmit/receive data registers.
+		*/
 		void write(int address, uint8_t value);
 
 		void run_for(HalfCycles);
+
+	private:
+		int divider_ = 1;
+		uint8_t status_ = 0x00;
 };
 
 }
