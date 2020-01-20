b0f7b762af0f3e008abc6942b8b83f8186992088
Thomas Harte <thomas.harte@gmail.com>
2020-01-19 21:40:30 -0500

    Adds a possible const.

--- NumberTheory/LFSR.hpp
+++ NumberTheory/LFSR.hpp
@@ -52,7 +52,7 @@ template <typename IntType = uint64_t, IntType polynomial = LSFRPolynomial<IntTy
 			determining the bit that was just shifted out.
 		*/
 		IntType next() {
-			auto result = value_ & 1;
+			const auto result = value_ & 1;
 			value_ = (value_ >> 1) ^ (result * polynomial);
 			return result;
 		}
