a681576d6cef2f0b6b82453566ba46359acd7644
Thomas Harte <thomas.harte@gmail.com>
2020-06-04 22:39:32 -0400

    Adds redraw logic.
    If you sit around and constantly reeize the window, you can now see that a machine is running.

--- OSBindings/Qt/scantargetwidget.cpp
+++ OSBindings/Qt/scantargetwidget.cpp
@@ -1,15 +1,23 @@
 #include "scantargetwidget.h"
 
+#include <QDebug>
+#include <QOpenGLContext>
+
 ScanTargetWidget::ScanTargetWidget(QWidget *parent) : QOpenGLWidget(parent) {}
 ScanTargetWidget::~ScanTargetWidget() {}
 
 void ScanTargetWidget::initializeGL() {
-	scanTarget = std::make_unique<Outputs::Display::OpenGL::ScanTarget>(defaultFramebufferObject());
 	glClearColor(0.5, 0.5, 1.0, 1.0);
+
+	qDebug() << "share context: " << bool(context()->shareGroup());
 }
 
 void ScanTargetWidget::paintGL() {
 	glClear(GL_COLOR_BUFFER_BIT);
+	if(scanTarget) {
+		scanTarget->update(width(), height());
+		scanTarget->draw(width(), height());
+	}
 }
 
 void ScanTargetWidget::resizeGL(int w, int h) {
@@ -17,5 +25,9 @@ void ScanTargetWidget::resizeGL(int w, int h) {
 }
 
 Outputs::Display::OpenGL::ScanTarget *ScanTargetWidget::getScanTarget() {
+	makeCurrent();
+	if(!scanTarget) {
+		scanTarget = std::make_unique<Outputs::Display::OpenGL::ScanTarget>(defaultFramebufferObject());
+	}
 	return scanTarget.get();
 }
