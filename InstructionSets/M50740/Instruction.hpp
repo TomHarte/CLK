819e9039abc5ce78bcedefd64ad2aeb8b403b4f3
Thomas Harte <thomas.harte@gmail.com>
2021-02-04 20:54:31 -0500

    Corrects printed target address for `ZeroPageRelative`.

--- InstructionSets/M50740/Instruction.hpp
+++ InstructionSets/M50740/Instruction.hpp
@@ -195,7 +195,7 @@ inline std::string address(AddressingMode addressing_mode, const uint8_t *operat
 		case AddressingMode::ImmediateZeroPage:		output << "#$" << NUM(operation[1]) << ", $"  << NUM(operation[2]);		break;
 		case AddressingMode::AccumulatorRelative:	output << "A, $"  << NUM4(2 + program_counter + int8_t(operation[1]));	break;
 		case AddressingMode::ZeroPageRelative:
-			output << "$" << NUM(operation[1]) << ", $"  << NUM4(2 + program_counter + int8_t(operation[2]));
+			output << "$" << NUM(operation[1]) << ", $"  << NUM4(3 + program_counter + int8_t(operation[2]));
 		break;
 	}
 #undef NUM4
