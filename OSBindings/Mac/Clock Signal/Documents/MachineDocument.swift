53ba0e67d1c54b42a04d36047a2d3242167d9cc3
Thomas Harte <thomas.harte@gmail.com>
2021-03-25 22:44:18 -0400

    Revert change to screenshot destination.
    For a sandboxed app, there's a lot more to it than this.

--- OSBindings/Mac/Clock Signal/Documents/MachineDocument.swift
+++ OSBindings/Mac/Clock Signal/Documents/MachineDocument.swift
@@ -606,8 +606,8 @@ class MachineDocument:
 
 		let filename = ("Clock Signal Screen Shot " + dateFormatter.string(from: Date()) + ".png").replacingOccurrences(of: "/", with: "-")
 			.replacingOccurrences(of: ":", with: ".")
-		let desktopURL = FileManager.default.urls(for: .desktopDirectory, in: .userDomainMask)[0]
-		let url = desktopURL.appendingPathComponent(filename)
+		let pictursURL = FileManager.default.urls(for: .picturesDirectory, in: .userDomainMask)[0]
+		let url = pictursURL.appendingPathComponent(filename)
 
 		// Obtain the machine's current display.
 		let imageRepresentation = self.machine.imageRepresentation
