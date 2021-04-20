a20f5528b790b7f955a7c79534b843ed78e62312
Thomas Harte <thomas.harte@gmail.com>
2021-04-19 22:04:49 -0400

    Switches default machine back to +2a.

--- Analyser/Static/ZXSpectrum/Target.hpp
+++ Analyser/Static/ZXSpectrum/Target.hpp
@@ -27,7 +27,7 @@ struct Target: public ::Analyser::Static::Target, public Reflection::StructImpl<
 		Plus3,
 	);
 
-	Model model = Model::Plus2;
+	Model model = Model::Plus2a;
 	bool should_hold_enter = false;
 
 	Target(): Analyser::Static::Target(Machine::ZXSpectrum) {
