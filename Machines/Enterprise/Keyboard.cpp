9411c37d233c4ff6ac129da5bc65457a18ff7350
Thomas Harte <thomas.harte@gmail.com>
2021-06-18 23:14:35 -0400

    Fleshes out the keyboard map.

--- Machines/Enterprise/Keyboard.cpp
+++ Machines/Enterprise/Keyboard.cpp
@@ -15,8 +15,28 @@ uint16_t KeyboardMapper::mapped_key_for_key(Inputs::Keyboard::Key key) const {
 	switch(key) {
 		default: break;
 
+		BIND(Backslash, Backslash);
+		BIND(CapsLock, Lock);
+		BIND(Tab, Tab);
+		BIND(Escape, Escape);
+		BIND(Hyphen, Hyphen);
+		BIND(Equals, Tilde);
+		BIND(Backspace, Erase);
+		BIND(Delete, Delete);
+		BIND(Semicolon, Semicolon);
+		BIND(Quote, Colon);
+		BIND(OpenSquareBracket, OpenSquareBracket);
+		BIND(CloseSquareBracket, CloseSquareBracket);
+
+		BIND(End, Stop);
+		BIND(Insert, Insert);
+		BIND(BackTick, At);
+
 		BIND(k1, k1);	BIND(k2, k2);	BIND(k3, k3);	BIND(k4, k4);	BIND(k5, k5);
 		BIND(k6, k6);	BIND(k7, k7);	BIND(k8, k8);	BIND(k9, k9);	BIND(k0, k0);
+
+		BIND(F1, F1);	BIND(F2, F2);	BIND(F3, F3);	BIND(F4, F4);
+		BIND(F5, F5);	BIND(F6, F6);	BIND(F7, F7);	BIND(F8, F8);
 
 		BIND(Q, Q);	BIND(W, W);	BIND(E, E);	BIND(R, R);	BIND(T, T);
 		BIND(Y, Y);	BIND(U, U);	BIND(I, I);	BIND(O, O);	BIND(P, P);
@@ -27,12 +47,16 @@ uint16_t KeyboardMapper::mapped_key_for_key(Inputs::Keyboard::Key key) const {
 		BIND(Z, Z);	BIND(X, X);	BIND(C, C);	BIND(V, V);
 		BIND(B, B);	BIND(N, N);	BIND(M, M);
 
+		BIND(FullStop, FullStop);
+		BIND(Comma, Comma);
+		BIND(ForwardSlash, ForwardSlash);
+
 		BIND(Space, Space);	BIND(Enter, Enter);
 
 		BIND(LeftShift, LeftShift);
 		BIND(RightShift, RightShift);
-		BIND(LeftOption, Option);
-		BIND(RightOption, Option);
+		BIND(LeftOption, Alt);
+		BIND(RightOption, Alt);
 		BIND(LeftControl, Control);
 		BIND(RightControl, Control);
 
