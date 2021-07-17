d1f3b5ed80ec1196308d64ff35242afe31784105
Thomas Harte <thomas.harte@gmail.com>
2021-07-16 21:07:12 -0400

    Obtains a Kickstart ROM, adds a 68000.

--- Machines/Amiga/Amiga.cpp
+++ Machines/Amiga/Amiga.cpp
@@ -10,26 +10,54 @@
 
 #include "../MachineTypes.hpp"
 
+#include "../../Processors/68000/68000.hpp"
+
 #include "../../Analyser/Static/Amiga/Target.hpp"
+
+#include "../Utility/MemoryPacker.hpp"
+#include "../Utility/MemoryFuzzer.hpp"
 
 namespace Amiga {
 
 class ConcreteMachine:
+	public CPU::MC68000::BusHandler,
 	public MachineTypes::ScanProducer,
 	public MachineTypes::TimedMachine,
 	public Machine {
 	public:
-		ConcreteMachine(const Analyser::Static::Amiga::Target &target, const ROMMachine::ROMFetcher &rom_fetcher) {
+		ConcreteMachine(const Analyser::Static::Amiga::Target &target, const ROMMachine::ROMFetcher &rom_fetcher) :
+			mc68000_(*this)
+		{
 			(void)target;
 			(void)rom_fetcher;
+
+			// Temporary: use a hard-coded Kickstart selection.
+			constexpr ROM::Name rom_name = ROM::Name::AmigaA500Kickstart13;
+			ROM::Request request(rom_name);
+			auto roms = rom_fetcher(request);
+			if(!request.validate(roms)) {
+				throw ROMMachine::Error::MissingROMs;
+			}
+			Memory::PackBigEndian16(roms.find(rom_name)->second, kickstart_);
 
 			// NTSC clock rate: 2*3.579545 = 7.15909Mhz.
 			// PAL clock rate: 7.09379Mhz.
 			set_clock_rate(7'093'790.0);
 		}
 
+		// MARK: - MC68000::BusHandler.
+		using Microcycle = CPU::MC68000::Microcycle;
+		HalfCycles perform_bus_operation(const CPU::MC68000::Microcycle &cycle, int is_supervisor) {
+			(void)cycle;
+			(void)is_supervisor;
+
+			return HalfCycles(0);
+		}
 
 	private:
+		CPU::MC68000::Processor<ConcreteMachine, true> mc68000_;
+		std::vector<uint8_t> kickstart_;
+
 		// MARK: - MachineTypes::ScanProducer.
 
 		void set_scan_target(Outputs::Display::ScanTarget *scan_target) final {
@@ -43,7 +71,7 @@ class ConcreteMachine:
 		// MARK: - MachineTypes::TimedMachine.
 
 		void run_for(const Cycles cycles) {
-			(void)cycles;
+			mc68000_.run_for(cycles);
 		}
 };
 
