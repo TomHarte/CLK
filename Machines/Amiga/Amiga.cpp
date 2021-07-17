7925dcc5a237fa99ec68299323bc2bd92c2f75ea
Thomas Harte <thomas.harte@gmail.com>
2021-07-16 20:49:12 -0400

    Advances far enough for the Amiga to be autonomous.

--- Machines/Amiga/Amiga.cpp
+++ Machines/Amiga/Amiga.cpp
@@ -8,18 +8,43 @@
 
 #include "Amiga.hpp"
 
+#include "../MachineTypes.hpp"
+
 #include "../../Analyser/Static/Amiga/Target.hpp"
 
 namespace Amiga {
 
 class ConcreteMachine:
+	public MachineTypes::ScanProducer,
+	public MachineTypes::TimedMachine,
 	public Machine {
 	public:
 		ConcreteMachine(const Analyser::Static::Amiga::Target &target, const ROMMachine::ROMFetcher &rom_fetcher) {
 			(void)target;
 			(void)rom_fetcher;
+
+			// NTSC clock rate: 2*3.579545 = 7.15909Mhz.
+			// PAL clock rate: 7.09379Mhz.
+			set_clock_rate(7'093'790.0);
 		}
 
+
+	private:
+		// MARK: - MachineTypes::ScanProducer.
+
+		void set_scan_target(Outputs::Display::ScanTarget *scan_target) final {
+			(void)scan_target;
+		}
+
+		Outputs::Display::ScanStatus get_scaled_scan_status() const {
+			return Outputs::Display::ScanStatus();
+		}
+
+		// MARK: - MachineTypes::TimedMachine.
+
+		void run_for(const Cycles cycles) {
+			(void)cycles;
+		}
 };
 
 }
