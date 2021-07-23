69d62560b47bddd9e52a171cdf416322f298f103
Thomas Harte <thomas.harte@gmail.com>
2021-07-22 22:00:33 -0400

    Adds comment to avoid potential future error.

--- Processors/68000/68000.hpp
+++ Processors/68000/68000.hpp
@@ -65,6 +65,8 @@ struct Microcycle {
 	/// If set, indicates a read. Otherwise, a write.
 	static constexpr OperationT Read 					= 1 << 2;
 
+	// Two-bit gap deliberately left here for PermitRead/Write below.
+
 	/// A NewAddress cycle is one in which the address strobe is initially low but becomes high;
 	/// this correlates to states 0 to 5 of a standard read/write cycle.
 	static constexpr OperationT NewAddress				= 1 << 5;
