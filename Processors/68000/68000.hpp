26f4758523ca5214fd0b645ff14a0a9fcca82985
Thomas Harte <thomas.harte@gmail.com>
2021-07-22 21:11:25 -0400

    Makes a further accommodation for PermitRead/Write.

--- Processors/68000/68000.hpp
+++ Processors/68000/68000.hpp
@@ -62,36 +62,36 @@ struct Microcycle {
 	/// Indicates that the address and both data select strobes are active.
 	static constexpr OperationT SelectWord				= 1 << 1;
 
+	/// If set, indicates a read. Otherwise, a write.
+	static constexpr OperationT Read 					= 1 << 2;
+
 	/// A NewAddress cycle is one in which the address strobe is initially low but becomes high;
 	/// this correlates to states 0 to 5 of a standard read/write cycle.
-	static constexpr OperationT NewAddress				= 1 << 2;
+	static constexpr OperationT NewAddress				= 1 << 5;
 
 	/// A SameAddress cycle is one in which the address strobe is continuously asserted, but neither
 	/// of the data strobes are.
-	static constexpr OperationT SameAddress			= 1 << 3;
+	static constexpr OperationT SameAddress				= 1 << 6;
 
 	/// A Reset cycle is one in which the RESET output is asserted.
-	static constexpr OperationT Reset					= 1 << 4;
-
-	/// If set, indicates a read. Otherwise, a write.
-	static constexpr OperationT Read 					= 1 << 5;
+	static constexpr OperationT Reset					= 1 << 7;
 
 	/// Contains the value of line FC0 if it is not implicit via InterruptAcknowledge.
-	static constexpr OperationT IsData 				= 1 << 6;
+	static constexpr OperationT IsData 					= 1 << 8;
 
 	/// Contains the value of line FC1 if it is not implicit via InterruptAcknowledge.
-	static constexpr OperationT IsProgram 				= 1 << 7;
+	static constexpr OperationT IsProgram 				= 1 << 9;
 
 	/// The interrupt acknowledge cycle is that during which the 68000 seeks to obtain the vector for
 	/// an interrupt it plans to observe. Noted on a real 68000 by all FCs being set to 1.
-	static constexpr OperationT InterruptAcknowledge	= 1 << 8;
+	static constexpr OperationT InterruptAcknowledge	= 1 << 10;
 
 	/// Represents the state of the 68000's valid memory address line — indicating whether this microcycle
 	/// is synchronised with the E clock to satisfy a valid peripheral address request.
-	static constexpr OperationT IsPeripheral 			= 1 << 9;
+	static constexpr OperationT IsPeripheral 			= 1 << 11;
 
 	/// Provides the 68000's bus grant line — indicating whether a bus request has been acknowledged.
-	static constexpr OperationT BusGrant				= 1 << 10;
+	static constexpr OperationT BusGrant				= 1 << 12;
 
 	/// Contains a valid combination of the various static constexpr int flags, describing the operation
 	/// performed by this Microcycle.
@@ -294,8 +294,11 @@ struct Microcycle {
 		return ((*address) & 0x00fffffe) >> 1;
 	}
 
-	static constexpr OperationT PermitRead = (1 << 11);
-	static constexpr OperationT PermitWrite = (1 << 12);
+	// PermitRead and PermitWrite are used as part of the read/write mask
+	// supplied to @c apply; they are picked to be small enough values that
+	// a byte can be used for storage.
+	static constexpr OperationT PermitRead 	= 1 << 3;
+	static constexpr OperationT PermitWrite	= 1 << 4;
 
 	/*!
 		Assuming this to be a cycle with a data select active, applies it to @c target
