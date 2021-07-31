5c1ac05170e2b524d744c9309e10ceeeb7ebe00c
Thomas Harte <thomas.harte@gmail.com>
2021-07-30 21:20:45 -0400

    Add documentation.

--- OSBindings/Mac/Clock Signal/Machine/CSROMFetcher.hpp
+++ OSBindings/Mac/Clock Signal/Machine/CSROMFetcher.hpp
@@ -9,4 +9,11 @@
 #include "ROMMachine.hpp"
 
 ROMMachine::ROMFetcher CSROMFetcher(ROM::Request *missing = nullptr);
-BOOL CSInstallROM(NSURL *);
+
+/// Loads the binary file located at @c url and then tests for whether it matches anything
+/// known to the ROM catalogue. If so then a copy of the ROM will be retained where it
+/// can later be found by the ROM fetcher returned by @c CSROMFetcher.
+///
+/// @returns @c true if the file was loaded successfully and matches something in
+/// the library; @c false otherwise.
+BOOL CSInstallROM(NSURL *url);
