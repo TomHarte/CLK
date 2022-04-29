e02d05e1f0db08aaf868fe770e45ffaf684222e3
Thomas Harte <thomas.harte@gmail.com>
2022-04-29 11:57:40 -0400

    Limit to a single semicolon on all usages of ReflectableEnum.

--- Reflection/Enum.hpp
+++ Reflection/Enum.hpp
@@ -21,7 +21,7 @@ namespace Reflection {
 
 #define ReflectableEnum(Name, ...)	\
 	enum class Name { __VA_ARGS__ };	\
-	constexpr static const char *__declaration##Name = #__VA_ARGS__;
+	constexpr static const char *__declaration##Name = #__VA_ARGS__
 
 #define EnumDeclaration(Name) #Name, __declaration##Name
 
