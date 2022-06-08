8cbf929671d719166253baa3a00b08f884f51d2d
Thomas Harte <thomas.harte@gmail.com>
2022-06-08 11:42:56 -0400

    Don't duplicate work that the RESET program already does.

--- Processors/68000Mk2/Implementation/68000Mk2Implementation.hpp
+++ Processors/68000Mk2/Implementation/68000Mk2Implementation.hpp
@@ -2785,7 +2785,6 @@ void Processor<BusHandler, dtack_is_implicit, permit_overrun, signal_will_perfor
 template <class BusHandler, bool dtack_is_implicit, bool permit_overrun, bool signal_will_perform>
 void Processor<BusHandler, dtack_is_implicit, permit_overrun, signal_will_perform>::reset() {
 	state_ = Reset;
-	status_.begin_exception(7);
 }
 
 }
