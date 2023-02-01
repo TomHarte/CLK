4cc34fd55713bfb76fc7259d8adcf6c9b6178f6e
Thomas Harte <thomas.harte@gmail.com>
2023-02-01 14:28:19 -0500

    Resolve GCC complaint.

--- Components/9918/Implementation/AccessEnums.hpp
+++ Components/9918/Implementation/AccessEnums.hpp
@@ -41,6 +41,7 @@ enum class ScreenMode {
 
 constexpr int pixels_per_byte(ScreenMode mode) {
 	switch(mode) {
+		default:
 		case ScreenMode::Blank:				return 0;
 		case ScreenMode::Text:				return 6;
 		case ScreenMode::MultiColour:		return 2;
