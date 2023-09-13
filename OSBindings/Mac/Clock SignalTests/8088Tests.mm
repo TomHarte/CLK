aa127d9a9f141e0157e7dc126737c9f690809bd3
Thomas Harte <thomas.harte@gmail.com>
2023-09-13 15:53:38 -0400

    Add an empty 8088 suite test class.

---
+++ OSBindings/Mac/Clock SignalTests/8088Tests.mm
@@ -0,0 +1,57 @@
+//
+//  8088Tests.m
+//  Clock SignalTests
+//
+//  Created by Thomas Harte on 13/09/2023.
+//  Copyright © 2023 Thomas Harte. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+#include <array>
+#include <cassert>
+
+#include <iostream>
+#include <fstream>
+
+#include "NSData+dataWithContentsOfGZippedFile.h"
+
+namespace {
+
+// The tests themselves are not duplicated in this repository;
+// provide their real path here.
+constexpr char TestSuiteHome[] = "/Users/tharte/Projects/ProcessorTests/8088/v1";
+
+}
+
+@interface i8088Tests : XCTestCase
+@end
+
+@implementation i8088Tests
+
+- (NSArray<NSString *> *)testFiles {
+	NSString *path = [NSString stringWithUTF8String:TestSuiteHome];
+
+	NSArray<NSString *> *files = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:path error:nil];
+	files = [files filteredArrayUsingPredicate:[NSPredicate predicateWithBlock:^BOOL(NSString* evaluatedObject, NSDictionary<NSString *,id> *) {
+		return [evaluatedObject hasSuffix:@"json.gz"];
+	}]];
+
+	NSMutableArray<NSString *> *fullPaths = [[NSMutableArray alloc] init];
+	for(NSString *file in files) {
+		[fullPaths addObject:[path stringByAppendingPathComponent:file]];
+	}
+
+	return fullPaths;
+}
+
+- (void)testDecoding {
+	for(NSString *file in [self testFiles]) {
+		NSData *data = [NSData dataWithContentsOfGZippedFile:file];
+		NSArray<NSDictionary *> *array = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+		NSLog(@"%@", array);
+		break;
+	}
+}
+
+@end
