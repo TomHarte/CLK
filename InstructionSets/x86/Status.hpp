7f6e3cf8b71872629b3658728c3f16a8054a12ac
Thomas Harte <thomas.harte@gmail.com>
2023-10-05 10:51:55 -0400

    Define the available flags.

---
+++ InstructionSets/x86/Status.hpp
@@ -0,0 +1,61 @@
+//
+//  Status.hpp
+//  Clock Signal
+//
+//  Created by Thomas Harte on 05/10/2023.
+//  Copyright © 2023 Thomas Harte. All rights reserved.
+//
+
+#ifndef InstructionSets_x86_Status_hpp
+#define InstructionSets_x86_Status_hpp
+
+
+namespace InstructionSet::x86 {
+
+namespace ConditionCode {
+
+//
+// Standard flags.
+//
+
+static constexpr uint32_t Carry				= 1 << 0;
+static constexpr uint32_t Parity			= 1 << 2;
+static constexpr uint32_t AuxiliaryCarry	= 1 << 4;
+static constexpr uint32_t Zero				= 1 << 6;
+static constexpr uint32_t Sign				= 1 << 7;
+static constexpr uint32_t Trap				= 1 << 8;
+static constexpr uint32_t Interrupt			= 1 << 9;
+static constexpr uint32_t Direction			= 1 << 10;
+static constexpr uint32_t Overflow			= 1 << 11;
+
+//
+// 80286+ additions.
+//
+
+static constexpr uint32_t IOPrivilege		= (1 << 12) | (1 << 13);
+static constexpr uint32_t NestedTask		= 1 << 14;
+
+//
+// 16-bit protected mode flags.
+//
+
+static constexpr uint32_t ProtectionEnable				= 1 << 16;
+static constexpr uint32_t MonitorProcessorExtension		= 1 << 17;
+static constexpr uint32_t ProcessorExtensionExtension	= 1 << 18;
+static constexpr uint32_t TaskSwitch					= 1 << 19;
+
+//
+// 32-bit protected mode flags.
+//
+
+static constexpr uint32_t Resume			= 1 << 16;
+static constexpr uint32_t VirtualMode		= 1 << 17;
+
+}
+
+struct Status {
+};
+
+}
+
+#endif /* InstructionSets_x86_Status_hpp */
