f9cbec668b3818504fd6eb173a3fd7ed7af8d8b8
Thomas Harte <thomas.harte@gmail.com>
2024-02-21 15:43:24 -0500

    Add empty shell for tests.

---
+++ OSBindings/Mac/Clock SignalTests/ARMDecoderTests.mm
@@ -0,0 +1,25 @@
+//
+//  ARMDecoderTests.m
+//  Clock Signal
+//
+//  Created by Thomas Harte on 16/02/2024.
+//  Copyright 2024 Thomas Harte. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+#include "../../../InstructionSets/ARM/OperationMapper.hpp"
+
+using namespace InstructionSet::ARM;
+
+@interface ARMDecoderTests : XCTestCase
+@end
+
+@implementation ARMDecoderTests
+
+- (void)testXYX {
+//	const auto intr = Instruction<Model::ARM2>(1);
+//	NSLog(@"%d", intr.operation());
+}
+
+@end
