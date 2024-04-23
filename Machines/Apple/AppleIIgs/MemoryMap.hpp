c485097eed68c9ecf5841a356dedc842adf10a5a
Thomas Harte <thomas.harte@gmail.com>
2024-04-22 22:06:09 -0400

    Fix bool combination.

--- Machines/Apple/AppleIIgs/MemoryMap.hpp
+++ Machines/Apple/AppleIIgs/MemoryMap.hpp
@@ -78,7 +78,7 @@ class MemoryMap {
 
 			const auto physical = physical_address(region, address);
 			assert(physical <= 0xff'ffff);
-			return shadow_pages_[(physical >> 10) & 127] & shadow_banks_[physical >> 17];
+			return shadow_pages_[(physical >> 10) & 127] && shadow_banks_[physical >> 17];
 		}
 		void write(const Region &region, uint32_t address, uint8_t value) {
 			if(!region.write) {
