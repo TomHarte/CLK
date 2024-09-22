ff6753fcdff03bdee9f103305a732d8cd52c1c1d
Thomas Harte <thomas.harte@gmail.com>
2024-09-21 21:12:04 -0400

    PEI: don't page wrap.

--- Processors/65816/Implementation/65816Storage.cpp
+++ Processors/65816/Implementation/65816Storage.cpp
@@ -713,7 +713,7 @@ struct CPU::WDC65816::ProcessorStorageConstructor {
 	static void stack_pei(AccessType, bool, const std::function<void(MicroOp)> &target) {
 		target(CycleFetchIncrementPC);			// DO.
 
-		target(OperationConstructDirect);
+		target(OperationConstructDirectLong);
 		target(CycleFetchPreviousPCThrowaway);	// IO.
 
 		target(CycleFetchIncrementData);		// AAL.
