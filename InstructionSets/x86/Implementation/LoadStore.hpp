d35efe3f32f3715b0d550616e5b136f9290eed02
Thomas Harte <thomas.harte@gmail.com>
2025-08-09 23:10:26 -0400

    Boil down descriptor attributes.

--- InstructionSets/x86/Implementation/LoadStore.hpp
+++ InstructionSets/x86/Implementation/LoadStore.hpp
@@ -154,7 +154,7 @@ void lldt(
 		);
 	};
 
-	if(ldt.type() != SegmentDescriptor::Type::LDTDescriptor) {
+	if(ldt.description().type != DescriptorType::LDT) {
 		throw Exception::exception<Vector::GeneralProtectionFault>(exception_code(source_segment));
 	}
 
