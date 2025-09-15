d0cc4e1557a57ae56b661d32ec300faceaaa0a34
Thomas Harte <thomas.harte@gmail.com>
2025-09-15 14:39:05 -0400

    Allow multiple copies of a program to be open.

--- OSBindings/Mac/Clock Signal/Document Controller/DocumentController.swift
+++ OSBindings/Mac/Clock Signal/Document Controller/DocumentController.swift
@@ -10,4 +10,19 @@ import Cocoa
 
 class DocumentController: NSDocumentController {
 	let joystickManager = CSJoystickManager()
+
+	// This causes a new NSDocument to be opened for every open request;
+	// the emulator won't check whether the file is already open and,
+	// if so, bring that window to the front, it'll just open a new window
+	// each time. My thinking at the time of implementation is that I want
+	// to implement internal networking on some of the more advanced machines
+	// which will likely mean letting them load multiple instances of the
+	// same network software, e.g. loading Stunt Car Racer onto multiple STs.
+	//
+	// I also think this is probably closer to expectations for an emulator,
+	// where current application state branches from the document loaded but
+	// then can go in a multitude of directions.
+	override func document(for url: URL) -> NSDocument? {
+		return nil
+	}
 }
