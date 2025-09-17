e863e61af8493eace67be7ba6ef35a1506381ef8
Thomas Harte <thomas.harte@gmail.com>
2025-09-17 11:46:53 -0400

    Remove dead code.

--- Machines/Acorn/BBCMicro/Keyboard.hpp
+++ Machines/Acorn/BBCMicro/Keyboard.hpp
@@ -16,25 +16,9 @@
 namespace BBCMicro {
 
 struct KeyboardMapper: public MachineTypes::MappedKeyboardMachine::KeyboardMapper {
-
 	uint16_t mapped_key_for_key(const Inputs::Keyboard::Key key) const override {
 		const auto found = key_map.find(key);
 		return found != key_map.end() ? found->second : MachineTypes::MappedKeyboardMachine::KeyNotMapped;
-//		switch(key) {
-//			default: break;
-//
-//			case Key::Escape:	return 0x70;
-//			case Key::Q:		return 0x10;
-//			case Key::F10:		return 0x20;
-//			case Key::k1:		return 0x30;
-//			case Key::CapsLock:	return 0x40;
-//			case Key::Tab:		return 0x60;
-//			case Key::LeftShift:
-//			case Key::RightShift:	return 0x00;
-//
-//		}
-//
-//		return MachineTypes::MappedKeyboardMachine::KeyNotMapped;
 	}
 
 private:
@@ -75,7 +59,6 @@ private:
 
 		{Key::Space, 0x62},
 	};
-
 };
 
 }
