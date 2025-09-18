ae774e88fa2a47d6f80064d23db1cd121e977e18
Thomas Harte <thomas.harte@gmail.com>
2025-09-17 21:42:42 -0400

    Add header for ADC.

---
+++ Components/uPD7002/uPD7002.hpp
@@ -0,0 +1,13 @@
+//
+//  uPD7002.hpp
+//  Clock Signal
+//
+//  Created by Thomas Harte on 17/09/2025.
+//  Copyright © 2025 Thomas Harte. All rights reserved.
+//
+
+#ifndef uPD7002_h
+#define uPD7002_h
+
+
+#endif /* uPD7002_h */
