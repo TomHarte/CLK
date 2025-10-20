2cdf6ac8f9495a2840dc57a49476b7f19f6bfc01
Thomas Harte <thomas.harte@gmail.com>
2025-10-20 13:16:03 -0400

    Add interrupt, RDY and instruction fetch logic.

--- Processors/6502Mk2/6502Mk2.hpp
+++ Processors/6502Mk2/6502Mk2.hpp
@@ -170,6 +170,70 @@ enum Flag: uint8_t {
 	IndexSize	= Break,
 };
 
+struct Flags {
+	/// Bit 7 is set if the negative flag is set; otherwise it is clear.
+	uint8_t negative_result = 0;
+
+	/// Non-zero if the zero flag is clear, zero if it is set.
+	uint8_t zero_result = 0;
+
+	/// Contains Flag::Carry.
+	uint8_t carry = 0;
+
+	/// Contains Flag::Decimal.
+	uint8_t decimal = 0;
+
+	/// Contains Flag::Overflow.
+	uint8_t overflow = 0;
+
+	/// Contains Flag::Interrupt, complemented.
+	uint8_t inverse_interrupt = 0;
+
+	/// Sets N and Z flags per the 8-bit value @c value.
+	void set_nz(const uint8_t value) {
+		zero_result = negative_result = value;
+	}
+
+	/// Sets N and Z flags per the 8- or 16-bit value @c value; @c shift should be 0 to indicate an 8-bit value or 8 to indicate a 16-bit value.
+	void set_nz(const uint16_t value, const int shift) {
+		negative_result = uint8_t(value >> shift);
+		zero_result = uint8_t(value | (value >> shift));
+	}
+
+	/// Sets the Z flag per the 8- or 16-bit value @c value; @c shift should be 0 to indicate an 8-bit value or 8 to indicate a 16-bit value.
+	void set_z(const uint16_t value, const int shift) {
+		zero_result = uint8_t(value | (value >> shift));
+	}
+
+	/// Sets the N flag per the 8- or 16-bit value @c value; @c shift should be 0 to indicate an 8-bit value or 8 to indicate a 16-bit value.
+	void set_n(const uint16_t value, const int shift) {
+		negative_result = uint8_t(value >> shift);
+	}
+
+	explicit operator uint8_t() const {
+		return
+			carry | overflow | (inverse_interrupt ^ Flag::Interrupt) | (negative_result & 0x80) |
+			(zero_result ? 0 : Flag::Zero) | Flag::Always | Flag::Break | decimal;
+	}
+
+	Flags() {
+		// Only the interrupt flag is defined upon reset but get_flags isn't going to
+		// mask the other flags so we need to do that, at least.
+		carry &= Flag::Carry;
+		decimal &= Flag::Decimal;
+		overflow &= Flag::Overflow;
+	}
+
+	Flags(const uint8_t flags) {
+		carry				= flags		& Flag::Carry;
+		negative_result		= flags		& Flag::Sign;
+		zero_result			= (~flags)	& Flag::Zero;
+		overflow			= flags		& Flag::Overflow;
+		inverse_interrupt	= (~flags)	& Flag::Interrupt;
+		decimal				= flags		& Flag::Decimal;
+	}
+};
+
 // MARK: - Type.
 
 enum Model {
@@ -182,6 +246,7 @@ enum Model {
 };
 constexpr bool is_8bit(const Model model) { return model <= Model::WDC65C02; }
 constexpr bool is_16bit(const Model model) { return model == Model::M65816; }
+constexpr bool is_65c02(const Model model) { return model >= Model::Synertek65C02; }
 
 // MARK: - Storage.
 
@@ -190,9 +255,13 @@ constexpr bool is_16bit(const Model model) { return model == Model::M65816; }
 */
 constexpr uint8_t JamOpcode = 0xf2;
 
-template <Model model, typename Enable = void> class Storage;
-template <Model model> class Storage<model, std::enable_if_t<is_8bit(model)>> {
+//template <Model model, typename Enable = void> class Storage;
+//template <Model model> class Storage<model, std::enable_if_t<true /* is_8bit(model) */>> {
+
+template <Model model, typename Traits> class Storage {
 public:
+	Storage(Traits::BusHandlerT &bus_handler) noexcept : bus_handler_(bus_handler) {}
+
 	uint16_t value_of(Register) const;
 	void set_value_of(Register, uint16_t);
 
@@ -211,7 +280,36 @@ public:
 	bool is_jammed() const;
 
 protected:
+	Traits::BusHandlerT &bus_handler_;
 	uint8_t opcode_, operand_;
+
+	uint8_t a_, x_, y_, s_;
+	RegisterPair16 pc_, operation_pc_;
+	Flags flags_;
+
+	Cycles cycles_;
+
+	enum ResumePoint {
+		FetchDecode,
+		Max,
+	};
+	int resume_point_ = ResumePoint::FetchDecode;
+
+	int return_from_ready_ = 0;
+	uint16_t ready_address_ = 0;
+
+	struct Inputs {
+		bool ready = false;
+
+		enum InterruptRequest: uint8_t {
+			Reset		= 0x80,
+			IRQ			= Flag::Interrupt,
+			NMI			= 0x20,
+
+			PowerOn		= 0x10,
+		};
+		uint8_t interrupt_requests = InterruptRequest::PowerOn;
+	} inputs_;
 };
 
 // MARK: - Base.
@@ -223,26 +321,17 @@ enum class PausePrecision {
 
 // TODO: concept to explain and verify Traits.
 template <Model model, typename Traits>
-class Processor: public Storage<model> {
-public:
+struct Processor: public Storage<model, Traits> {
+	inline void run_for(Cycles);
+
 	/*!
 		**FOR TEST CASES ONLY:** forces the processor into a state where
 		the next thing it intends to do is fetch a new opcode.
 	*/
 	inline void restart_operation_fetch();
-	inline void run_for(Cycles);
 
 private:
-	Cycles cycles_;
-
-	enum ResumePoint {
-		FetchDecodeExecute,
-		Max,
-	};
-	int resume_point_ = ResumePoint::FetchDecodeExecute;
-
-	int return_from_ready_ = 0;
-	uint16_t ready_address_ = 0;
+	using Storage = Storage<model, Traits>;
 };
 
 }
--- Processors/6502Mk2/Implementation/6502.hpp
+++ Processors/6502Mk2/Implementation/6502.hpp
@@ -8,86 +8,146 @@
 
 #pragma once
 
+#include <cassert>
+
 namespace CPU::MOS6502Mk2 {
 
 template <Model model, typename Traits>
 void Processor<model, Traits>::restart_operation_fetch() {
-	resume_point_ = ResumePoint::FetchDecode;
+	Storage::resume_point_ = Storage::ResumePoint::FetchDecode;
 }
 
 template <Model model, typename Traits>
 void Processor<model, Traits>::run_for(const Cycles cycles) {
-	cycles_ += cycles;
-	if(cycles_ <= Cycles(0)) return;
+	Storage::cycles_ += cycles;
+	if(Storage::cycles_ <= Cycles(0)) return;
 
 	#define restore_point()	(__COUNTER__ + ResumePoint::Max)
 
-	#define test_cycles(precision) 								\
-		if constexpr (Traints::pause_precision >= precision) {	\
-			static constexpr int location = restore_point();	\
-			if(cycles_ <= Cycles(0)) {							\
-				resume_point_ = location;						\
+	#define test_cycles(precision) {							\
+		static constexpr int test_location = restore_point();	\
+		if constexpr (Traits::pause_precision >= precision) {	\
+			if(Storage::cycles_ <= Cycles(0)) {					\
+				Storage::resume_point_ = test_location;			\
 				return;											\
 			}													\
-																\
-			[[fallthrough]] case location:						\
-		}
+		}														\
+		[[fallthrough]]; case test_location: (void)0;			\
+	}
 
-	#define read(type, addr, value)	{						\
-		static_assert(is_read(type));						\
-		static constexpr int location = restore_point();	\
-															\
-		if(Traits::uses_ready_line && ready_is_active_) {	\
-			return_from_ready_ = location;					\
-			resume_point_ = ResumePoint::SpinReady;			\
-			goto spin_ready;								\
-		}													\
-															\
-		[[fallthrough]] case location:						\
-		cycles_ -= bus_handler_.perform<type>(addr, value);	\
-		test_cycles(PausePrecision::AnyCycle)				\
+	// TODO: find a way not to generate a restore point if Traits::uses_ready_line is false.
+	#define read(type, addr, value)	{											\
+		static_assert(is_read(type));											\
+		static constexpr int location = restore_point();						\
+																				\
+		if(Traits::uses_ready_line && Storage::inputs_.ready) {					\
+			Storage::return_from_ready_ = location;								\
+			goto spin_ready;													\
+		}																		\
+																				\
+		[[fallthrough]]; case location:											\
+		Storage::cycles_ -= Storage::bus_handler_.perform<type>(addr, value);	\
+		test_cycles(PausePrecision::AnyCycle)									\
 	}
 
 	// TODO: test RDY if a suitable-advanced 6502.
-	#define write(type, addr, value)	{					\
-		static_assert(is_write(type));						\
-		cycles_ -= bus_handler_.perform<type>(addr, value);	\
-		test_cycles(PausePrecision::AnyCycle)				\
+	#define write(type, addr, value)	{										\
+		static_assert(is_write(type));											\
+		Storage::cycles_ -= Storage::bus_handler_.perform<type>(addr, value);	\
+		test_cycles(PausePrecision::AnyCycle)									\
 	}
 
-//	while(true) switch(resume_point_) {
-//		//
-//		// Spin endlessly while RDY is active, repeating ready_address_ on the address bus.
-//		//
-//		spin_ready:
-//			cycles_ -= bus_handler_.perform<BusOperation::Ready>(Address::Literal(ready_address_), Data::NoValue());
-//			test_cycles(PausePrecision::AnyCycle);
-//			if(ready_is_active_) {
-//				goto spin_ready;
-//			}
-//
-//			resume_point_ = return_from_ready_;
-//			break;
-//
-//		//
-//		// Grab the opcode and an initial byte of operand, then decode and segue into the proper
-//		// addressing mode logic.
-//		//
-//		fetch_decode:
-//		case ResumePoint::FetchDecode:
-//
-//			// Pause precision will always be at least operation by operation.
-//			if(cycles_ <= Cycles(0)) {
-//				resume_point_ = ResumePoint::FetchDecode;
-//				return;
-//			}
-//
-//			read(Address::Literal(pc_), opcode_);
-//			++pc_;
-//			read(Address::Literal(pc_), operand_);
-//
-//			// TODO: decode. Kind of important!
-//	}
+	using ResumePoint = Storage::ResumePoint;
+	using InterruptRequests = Storage::Inputs::InterruptRequests;
+
+	while(true) switch(Storage::resume_point_) {
+		// MARK: - Fetch/decode.
+		fetch_decode:
+		case ResumePoint::FetchDecode:
+
+			// Pause precision will always be at least operation by operation.
+			if(Storage::cycles_ <= Cycles(0)) {
+				Storage::resume_point_ = ResumePoint::FetchDecode;
+				return;
+			}
+
+			if(Storage::inputs_.interrupt_requests) {
+				goto interrupt;
+			}
+
+			read(BusOperation::ReadOpcode, Address::Literal(Storage::pc_), Storage::opcode_);
+			++Storage::pc_;
+			read(BusOperation::Read, Address::Literal(Storage::pc_), Storage::operand_);
+
+			// TODO: decode. Kind of important!
+
+
+		// MARK: - Spin on RDY.
+		spin_ready:
+			Storage::cycles_ -= Storage::bus_handler_.perform<BusOperation::Ready>(
+				Address::Literal{Storage::ready_address_},
+				Data::NoValue{}
+			);
+			test_cycles(PausePrecision::AnyCycle);
+			if(Storage::inputs_.ready) {
+				goto spin_ready;
+			}
+
+			Storage::resume_point_ = Storage::return_from_ready_;
+			break;
+
+		// MARK: - NMI/IRQ/Reset.
+		interrupt:
+			read(BusOperation::Read, Address::Literal(Storage::pc_), Storage::operand_);
+			read(BusOperation::Read, Address::Literal(Storage::pc_), Storage::operand_);
+
+			if(Storage::inputs_.interrupt_requests & (InterruptRequests::Reset | InterruptRequests::PowerOn)) {
+				Storage::inputs_.interrupt_requests &= ~InterruptRequests::PowerOn;
+				goto reset;
+			}
+			assert(Storage::inputs_.interrupt_requests & (InterruptRequests::IRQ | InterruptRequests::NMI));
+
+			--Storage::s_;
+			read(BusOperation::Write, Address::Stack(Storage::s_), Storage::pc_.halves.high);
+			--Storage::s_;
+			read(BusOperation::Write, Address::Stack(Storage::s_), Storage::pc_.halves.low);
+			--Storage::s_;
+			read(BusOperation::Write, Address::Stack(Storage::s_), static_cast<uint8_t>(Storage::flags_) & ~Flag::Break);
+
+			Storage::flags_.inverse_interrupt = 0;
+			if constexpr (is_65c02(model)) {
+				Storage::flags_.decimal = 0;
+			}
+
+			if(Storage::inputs_.interrupt_requests & InterruptRequests::NMI) {
+				goto nmi;
+			}
+
+			read(BusOperation::Read, Address::Vector(0xfe), Storage::pc_.halves.low);
+			read(BusOperation::Read, Address::Vector(0xff), Storage::pc_.halves.high);
+			goto fetch_decode;
+
+		nmi:
+			read(BusOperation::Read, Address::Vector(0xfa), Storage::pc_.halves.low);
+			read(BusOperation::Read, Address::Vector(0xfb), Storage::pc_.halves.high);
+			goto fetch_decode;
+
+		reset:
+			--Storage::s_;
+			read(BusOperation::Read, Address::Stack(Storage::s_), Storage::operand_);
+			--Storage::s_;
+			read(BusOperation::Read, Address::Stack(Storage::s_), Storage::operand_);
+			--Storage::s_;
+			read(BusOperation::Read, Address::Stack(Storage::s_), Storage::operand_);
+
+			Storage::flags_.inverse_interrupt = 0;
+			if constexpr (is_65c02(model)) Storage::flags_.decimal = 0;
+
+			read(BusOperation::Read, Address::Vector(0xfc), Storage::pc_.halves.low);
+			read(BusOperation::Read, Address::Vector(0xfd), Storage::pc_.halves.high);
+
+			goto fetch_decode;
+	}
 
 	#undef write
 	#undef read
