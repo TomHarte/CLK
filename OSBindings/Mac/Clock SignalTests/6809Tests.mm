15600de465cd6e68a270e9827250f73288baf2fb
Thomas Harte <thomas.harte@gmail.com>
2026-03-04 17:39:26 -0500

    Add missing test file.

---
+++ OSBindings/Mac/Clock SignalTests/6809Tests.mm
@@ -0,0 +1,20 @@
+//
+//  68000BCDTests.m
+//  Clock SignalTests
+//
+//  Created by Thomas Harte on 29/06/2019.
+//
+//  Largely ported from the tests of the Portable 68k Emulator.
+//
+
+#import <XCTest/XCTest.h>
+
+#include "Processors/6809/6809.hpp"
+
+@interface M6809Tests : XCTestCase
+@end
+
+@implementation M6809Tests
+
+
+@end
