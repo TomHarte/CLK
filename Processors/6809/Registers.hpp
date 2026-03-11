21d2ab9179fb80ec315042b82e37b4a4b55d6f4f
Thomas Harte <thomas.harte@gmail.com>
2026-03-11 10:06:05 -0400

    Flesh out condition codes.

--- Processors/6809/Registers.hpp
+++ Processors/6809/Registers.hpp
@@ -33,37 +33,72 @@ struct ConditionCodeRegister {
 	template <ConditionCode code>
 	bool condition() const {
 		switch(code) {
-			case ConditionCode::Negative:	return sign_ & 0x80;
+			case ConditionCode::Entire:		return entire_;
+			case ConditionCode::FIRQMask:	return firq_;
+			case ConditionCode::HalfCarry:	return half_carry_;
+			case ConditionCode::IRQMask:	return irq_;
+			case ConditionCode::Negative:	return negative_ & 0x80;
 			case ConditionCode::Zero:		return !zero_;
-			case ConditionCode::Overflow:	return overflow_;
+			case ConditionCode::Overflow:	return overflow_ & 0x80;
+			case ConditionCode::Carry:		return carry_;
 		}
-	}
-
-	void set_nz(const uint8_t value) {
-		sign_ = zero_ = value;
 	}
 
 	template <ConditionCode code>
 	void set(const bool value) {
 		switch(code) {
-			case ConditionCode::Negative:	sign_ = value ? 0xff : 0x00;	break;
-			case ConditionCode::Zero:		zero_ = value ? 0x00 : 0xff;	break;
-			case ConditionCode::Overflow:	overflow_ = value;				break;
+			case ConditionCode::Entire:		entire_ = value;					break;
+			case ConditionCode::FIRQMask:	firq_ = value;						break;
+			case ConditionCode::HalfCarry:	half_carry_ = value ? 0x10 : 0x00;	break;
+			case ConditionCode::IRQMask:	irq_ = value;						break;
+			case ConditionCode::Negative:	negative_ = value ? 0xff : 0x00;	break;
+			case ConditionCode::Zero:		zero_ = value ? 0x00 : 0xff;		break;
+			case ConditionCode::Overflow:	overflow_ = value ? 0xff : 0x00;	break;
+			case ConditionCode::Carry:		carry_ = value ? 0x01 : 0x00;		break;
 		}
 	}
 
 	uint8_t operator =(const uint8_t rhs) {
+		entire_ = rhs & ConditionCode::Entire;
+		firq_ = rhs & ConditionCode::FIRQMask;
+		half_carry_ = uint8_t((rhs & ConditionCode::HalfCarry) >> 1);
+		irq_ = rhs & ConditionCode::IRQMask;
+		negative_ = uint8_t((rhs & ConditionCode::Negative) << 4);
+		zero_ = !(rhs & ConditionCode::Zero);
+		overflow_ = uint8_t((rhs & ConditionCode::Overflow) << 6);
+		carry_ = rhs & ConditionCode::Carry;
 		return rhs;
 	}
 
 	operator uint8_t() const {
-		return 0x00;
+		return uint8_t(
+			(entire_ ? ConditionCode::Entire : 0) |
+			(firq_ ? ConditionCode::FIRQMask : 0) |
+			(half_carry_ << 1) |
+			(irq_ ? ConditionCode::IRQMask : 0) |
+			((negative_ & 0x80) ? ConditionCode::Negative : 0) |
+			(zero_ ? 0 : ConditionCode::Zero) |
+			((overflow_ & 0x80) ? ConditionCode::Overflow : 0) |
+			carry_
+		);
 	}
 
+	void set_nz(const uint8_t value) {
+		negative_ = zero_ = value;
+	}
+
+	uint8_t carry() const { return carry_; }
+	uint8_t half_carry() const { return half_carry_; }
+
 private:
-	uint8_t sign_ = 0;
+	uint8_t negative_ = 0;
 	uint8_t zero_ = 0;
-	bool overflow_ = false;
+	uint8_t overflow_ = 0;
+	uint8_t carry_ = 0;
+	bool irq_ = true;
+	uint8_t half_carry_ = 0;
+	bool firq_ = true;
+	bool entire_ = false;
 };
 
 enum R8 {
