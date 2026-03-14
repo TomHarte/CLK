9f47347977f232d347359d5bcc498e807ca88dc7
Thomas Harte <thomas.harte@gmail.com>
2026-03-13 21:14:27 -0400

    Insert a final filter.

--- InstructionSets/6809/OperationMapper.hpp
+++ InstructionSets/6809/OperationMapper.hpp
@@ -132,11 +132,29 @@ enum class Page {
 	Page0, Page1, Page2,
 };
 
+/*!
+	Utility of geneal usefulness: whatever the @c OperationMapper produces, just return it on the stack.
+*/
 struct OperationReturner {
 	template <Operation operation, AddressingMode mode> auto schedule() {
 		return std::make_pair(operation, mode);
 	}
 };
+
+template <Operation operation, AddressingMode mode, typename SchedulerT>
+auto complete(SchedulerT &s) {
+	// To do:
+	//
+	//	(1) upgrade Immediate8 to Immediate16 where justified;
+	//	(2) commute [Direct/Extended/Indexed]Read to [Read/Write/Modify] as appropriate.
+
+	switch(mode) {
+		using enum AddressingMode;
+
+		default:			return s.template schedule<operation, mode>();
+		case Immediate8:	return s.template schedule<operation, is_16bit<operation>() ? Immediate16 : Immediate8>();
+	}
+}
 
 /*!
 	Calls @c scheduler.schedule<Operation,AddressingMode> to describe the instruction
@@ -144,22 +162,6 @@ struct OperationReturner {
 */
 template <Page page> struct OperationMapper {
 	template <int i, typename SchedulerT> auto dispatch(SchedulerT &scheduler);
-};
-
-template <Operation operation, uint8_t opcode>
-constexpr AddressingMode mode() {
-	using AM = AddressingMode;
-	constexpr AddressingMode modes[] = {
-		AM::Immediate8, AM::Direct, AM::Indexed, AM::Extended
-	};
-	constexpr auto prima_facie_mode = modes[(opcode >> 4) & 3];
-	constexpr bool is_16 = is_16bit<operation>();
-
-	if constexpr (prima_facie_mode != AM::Immediate8 || !is_16) {
-		return prima_facie_mode;
-	} else {
-		return AM::Immediate16;
-	}
 };
 
 template <>
@@ -182,8 +184,13 @@ auto OperationMapper<Page::Page0>::dispatch(SchedulerT &s) {
 		//	0xc7:	STB immediate
 		//	0xcd:	STD immediate
 		//	0xcf:	STU immediate.
-		return s.template schedule<O::None, AM::Illegal>();
+		return complete<O::None, AM::Illegal>(s);
 	}
+
+	static constexpr AddressingMode modes[] = {
+		AM::Immediate8, AM::Direct, AM::Indexed, AM::Extended
+	};
+	static constexpr auto mode = modes[(i >> 4) & 3];
 
 	switch(upper) {
 		default: break;
@@ -199,14 +206,14 @@ auto OperationMapper<Page::Page0>::dispatch(SchedulerT &s) {
 				AM::Illegal,	AM::Inherent,	AM::Immediate8,	AM::Illegal,
 				AM::Immediate8,	AM::Inherent,	AM::Immediate8,	AM::Immediate8,
 			};
-			return s.template schedule<operations[lower], specific_modes[lower]>();
+			return complete<operations[lower], specific_modes[lower]>(s);
 		}
 		case 0x2: {
 			static constexpr Operation operations[] = {
 				O::BRA,		O::BRN,		O::BHI,		O::BLS,		O::BCC,		O::BCS,		O::BNE,		O::BEQ,
 				O::BVC,		O::BVS,		O::BPL,		O::BMI,		O::BGE,		O::BLT,		O::BGT,		O::BLE,
 			};
-			return s.template schedule<operations[lower], AM::Relative8>();
+			return complete<operations[lower], AM::Relative8>(s);
 		}
 		case 0x3: {
 			static constexpr Operation operations[] = {
@@ -216,16 +223,16 @@ auto OperationMapper<Page::Page0>::dispatch(SchedulerT &s) {
 			static constexpr auto op = operations[lower];
 			switch(lower) {
 				case 0x0:	case 0x1:	case 0x2:	case 0x3:
-				return s.template schedule<op, AM::Indexed>();
+				return complete<op, AM::Indexed>(s);
 
 				case 0x4:	case 0x5:	case 0x6:	case 0x7:	case 0xc:
-				return s.template schedule<op, AM::Immediate8>();
+				return complete<op, AM::Immediate8>(s);
 
 				case 0x8:
-				return s.template schedule<op, AM::Illegal>();
+				return complete<op, AM::Illegal>(s);
 
 				default:
-				return s.template schedule<op, AM::Inherent>();
+				return complete<op, AM::Inherent>(s);
 			}
 		}
 		case 0x4: {
@@ -234,7 +241,7 @@ auto OperationMapper<Page::Page0>::dispatch(SchedulerT &s) {
 				O::LSLA,	O::ROLA,	O::DECA,	O::None,	O::INCA,	O::TSTA,	O::None,	O::CLRA,
 			};
 			static constexpr auto op = operations[lower];
-			return s.template schedule<op, op == O::None ? AM::Illegal : AM::Inherent>();
+			return complete<op, op == O::None ? AM::Illegal : AM::Inherent>(s);
 		}
 		case 0x5: {
 			static constexpr Operation operations[] = {
@@ -242,7 +249,7 @@ auto OperationMapper<Page::Page0>::dispatch(SchedulerT &s) {
 				O::LSLB,	O::ROLB,	O::DECB,	O::None,	O::INCB,	O::TSTB,	O::None,	O::CLRB,
 			};
 			static constexpr auto op = operations[lower];
-			return s.template schedule<op, op == O::None ? AM::Illegal : AM::Inherent>();
+			return complete<op, op == O::None ? AM::Illegal : AM::Inherent>(s);
 		}
 		case 0x0: case 0x6:	case 0x7: {
 			static constexpr Operation operations[] = {
@@ -250,22 +257,22 @@ auto OperationMapper<Page::Page0>::dispatch(SchedulerT &s) {
 				O::LSL,		O::ROL,		O::DEC,		O::None,	O::INC,		O::TST,		O::JMP,		O::CLR,
 			};
 			static constexpr auto op = operations[lower];
-			return s.template schedule<op, op == O::None ? AM::Illegal : upper == 0 ? AM::Direct : mode<op, i>()>();
+			return complete<op, op == O::None ? AM::Illegal : upper == 0 ? AM::Direct : mode>(s);
 		}
 		case 0x8:	case 0x9:	case 0xa:	case 0xb: {
 			static constexpr Operation operations[] = {
 				O::SUBA,	O::CMPA,	O::SBCA,	O::SUBD,	O::ANDA,	O::BITA,	O::LDA,		O::STA,
 				O::EORA,	O::ADCA,	O::ORA,		O::ADDA,	O::CMPX,	O::JSR,		O::LDX,		O::STX,
 			};
-			if(i == 0x8d)	return s.template schedule<O::BSR, AM::Relative8>();
-			else			return s.template schedule<operations[lower], mode<operations[lower], i>()>();
+			if(i == 0x8d)	return complete<O::BSR, AM::Relative8>(s);
+			else			return complete<operations[lower], mode>(s);
 		}
 		case 0xc:	case 0xd:	case 0xe:	case 0xf: {
 			static constexpr Operation operations[] = {
 				O::SUBB,	O::CMPB,	O::SBCB,	O::ADDD,	O::ANDB,	O::BITB,	O::LDB,		O::STB,
 				O::EORB,	O::ADCB,	O::ORB,		O::ADDB,	O::LDD,		O::STD,		O::LDU,		O::STU,
 			};
-			return s.template schedule<operations[lower], mode<operations[lower], i>()>();
+			return complete<operations[lower], mode>(s);
 		}
 	}
 	__builtin_unreachable();
@@ -276,6 +283,11 @@ template <int i, typename SchedulerT>
 auto OperationMapper<Page::Page1>::dispatch(SchedulerT &s) {
 	using AM = AddressingMode;
 	using O = Operation;
+
+	static constexpr AddressingMode modes[] = {
+		AM::Immediate8, AM::Direct, AM::Indexed, AM::Extended
+	};
+	static constexpr auto mode = modes[(i >> 4) & 3];
 
 	if constexpr (i >= 0x21 && i < 0x30) {
 		static constexpr Operation operations[] = {
@@ -288,22 +300,22 @@ auto OperationMapper<Page::Page1>::dispatch(SchedulerT &s) {
 		case 0x3f:	return s.template schedule<O::SWI2, AM::Inherent>();
 
 		case 0x83:	case 0x93:	case 0xa3:	case 0xb3:
-		return s.template schedule<O::CMPD, mode<O::CMPD, i>()>();
+		return complete<O::CMPD, mode>(s);
 
 		case 0x8c:	case 0x9c:	case 0xac:	case 0xbc:
-		return s.template schedule<O::CMPY, mode<O::CMPY, i>()>();
+		return complete<O::CMPY, mode>(s);
 
 		case 0x8e:	case 0x9e:	case 0xae:	case 0xbe:
-		return s.template schedule<O::LDY, mode<O::LDY, i>()>();
+		return complete<O::LDY, mode>(s);
 
 		case 0x9f:	case 0xaf:	case 0xbf:
-		return s.template schedule<O::STY, mode<O::STY, i>()>();
+		return complete<O::STY, mode>(s);
 
 		case 0xce:	case 0xde:	case 0xee:	case 0xfe:
-		return s.template schedule<O::LDS, mode<O::LDS, i>()>();
+		return complete<O::LDS, mode>(s);
 
 		case 0xdf:	case 0xef:	case 0xff:
-		return s.template schedule<O::STS, mode<O::STS, i>()>();
+		return complete<O::STS, mode>(s);
 	}
 	__builtin_unreachable();
 }
@@ -314,15 +326,20 @@ auto OperationMapper<Page::Page2>::dispatch(SchedulerT &s) {
 	using AM = AddressingMode;
 	using O = Operation;
 
+	static constexpr AddressingMode modes[] = {
+		AM::Immediate8, AM::Direct, AM::Indexed, AM::Extended
+	};
+	static constexpr auto mode = modes[(i >> 4) & 3];
+
 	switch(i) {
-		default:	return s.template schedule<O::None, AM::Illegal>();
-		case 0x3f:	return s.template schedule<O::SWI3, AM::Inherent>();
+		default:	return complete<O::None, AM::Illegal>(s);
+		case 0x3f:	return complete<O::SWI3, AM::Inherent>(s);
 
 		case 0x83:	case 0x93:	case 0xa3:	case 0xb3:
-		return s.template schedule<O::CMPU, mode<O::CMPU, i>()>();
+		return complete<O::CMPU, mode>(s);
 
 		case 0x8c:	case 0x9c:	case 0xac:	case 0xbc:
-		return s.template schedule<O::CMPS, mode<O::CMPS, i>()>();
+		return complete<O::CMPS, mode>(s);
 	}
 	__builtin_unreachable();
 }
