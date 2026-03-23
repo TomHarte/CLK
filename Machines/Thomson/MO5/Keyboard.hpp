36c460838697c8c6626552379fe581b51bf77c48
Thomas Harte <thomas.harte@gmail.com>
2026-03-22 22:14:07 -0400

    Attempt a full keyboard mapping.

--- Machines/Thomson/MO5/Keyboard.hpp
+++ Machines/Thomson/MO5/Keyboard.hpp
@@ -18,50 +18,59 @@ enum Key: uint16_t {
 struct KeyboardMapper: public MachineTypes::MappedKeyboardMachine::KeyboardMapper {
 	uint16_t mapped_key_for_key(const Inputs::Keyboard::Key key) const {
 		switch(key) {
-			default:	return 1;
+			default:	return MachineTypes::MappedKeyboardMachine::KeyNotMapped;
 
 			using enum Inputs::Keyboard::Key;
-			case k4:	return 23;	// 4
-			case N:		return 0;
-			case J:		return 2;
-			case H:		return 3;
-			case U:		return 4;
-			case Y:		return 5;
-			case k7:	return 6;
-			case k6:	return 7;
-			case Comma:	return 9;
-			case K:		return 10;
-			case G:		return 11;
-			case I:		return 12;
-			case T:		return 13;
-			case k8:	return 14;
-			case k5:	return 15;
-			case FullStop:	return 16;
-//			case k2:	return 17;	// [go to top left?]
-			case L:		return 18;
+			case k0:	return 0x1e;		case k1:	return 0x2f;
+			case k2:	return 0x27;		case k3:	return 0x1f;
+			case k4:	return 0x17;		case k5:	return 0x0f;
+			case k6:	return 0x07;		case k7:	return 0x06;
+			case k8:	return 0x0e;		case k9:	return 0x16;
 
-			case F:		return 19;
-			case O:		return 20;
-			case R:		return 21;
-			case k9:	return 22;
-//			case k4:	return 23;	[duplicate?]
-//			case k3:	return 24;	// @
+			case Q:		return 0x2d;		case W:		return 0x25;
+			case E:		return 0x1d;		case R:		return 0x15;
+			case T:		return 0x0d;		case Y:		return 0x05;
+			case U:		return 0x04;		case I:		return 0x0c;
+			case O:		return 0x14;		case P:		return 0x1c;
+			case A:		return 0x2b;		case S:		return 0x23;
+			case D:		return 0x1b;		case F:		return 0x13;
+			case G:		return 0x0b;		case H:		return 0x03;
+			case J:		return 0x02;		case K:		return 0x0a;
+			case L:		return 0x12;		case Z:		return 0x30;
+			case X:		return 0x28;		case C:		return 0x32;
+			case V:		return 0x2a;		case B:		return 0x22;
+			case N:		return 0x00;		case M:		return 0x1a;
 
-			case Space:		return 25;	// [space]
-			case M:		return 26;	// M
-			case D:		return 27;	// D
-			case P:		return 28;	// P
-			case E:		return 29;	// E
+			case FullStop:	return 0x10;
+			case Hyphen:	return 0x26;
+			case Equals:	return 0x2e;	// +
 
+			case OpenSquareBracket:	return 0x18;	// @
+			case Semicolon:			return 0x1a;	// M
 
-			case Z:		return 30;	// [space]
-			case X:		return 31;	// M
-			case C:		return 32;	// D
-			case V:		return 33;	// P
-			case B:		return 34;	// E
-//			case M:		return 35;	// 0
+			case CloseSquareBracket:
+			case Quote:		return 0x2c;	// Asterisk.
+
+			case Space:		return 0x20;
+			case Enter:		return 0x34;
+			case Backspace:	return 0x36;
+			case Tab:		return 0x37;
+
+			case Up:		return 0x31;	case Down:	return 0x21;
+			case Left:		return 0x29;	case Right:	return 0x19;
+
+			case LeftShift:
+			case RightShift:	return 0x38;
+			case LeftControl:
+			case RightControl:	return 0x35;
+			case LeftOption:
+			case RightOption:	return 0x33;
+			case LeftMeta:
+			case RightMeta:		return 0x39;
+
+			// TODO: INS and EFF from the lower right, which I think are 0x09 and 0x11?
+			// And where the heck is forward slash?
 		}
-
 	}
 };
 
