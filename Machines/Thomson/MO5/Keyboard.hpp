9841c816f24fb767532ff66e1bd69a0e2514410f
Thomas Harte <thomas.harte@gmail.com>
2026-03-27 10:23:35 -0400

    Pull out native key names.

--- Machines/Thomson/MO5/Keyboard.hpp
+++ Machines/Thomson/MO5/Keyboard.hpp
@@ -9,10 +9,48 @@
 #pragma once
 
 #include "Machines/KeyboardMachine.hpp"
+#include "Machines/Utility/Typer.hpp"
 
 namespace MO5 {
 
 enum Key: uint16_t {
+	Key0	= 0x1e,		Key1	= 0x2f,		Key2	= 0x27,		Key3 	= 0x1f,
+	Key4	= 0x17,		Key5	= 0x0f,		Key6	= 0x07,		Key7 	= 0x06,
+	Key8	= 0x0e,		Key9	= 0x16,
+
+	KeyA 	= 0x2d,		KeyZ	= 0x25,		KeyE 	= 0x1d,		KeyR	= 0x15,
+	KeyT	= 0x0d,		KeyY	= 0x05,		KeyU	= 0x04,		KeyI	= 0x0c,
+	KeyO	= 0x14,		KeyP	= 0x1c,		KeyQ	= 0x2b,		KeyS	= 0x23,
+	KeyD	= 0x1b,		KeyF	= 0x13,		KeyG	= 0x0b,		KeyH	= 0x03,
+	KeyJ	= 0x02,		KeyK	= 0x0a,		KeyL	= 0x12,		KeyW	= 0x30,
+	KeyX	= 0x28,		KeyC	= 0x32,		KeyV	= 0x2a,		KeyB	= 0x22,
+	KeyN	= 0x00,		KeyM	= 0x1a,
+
+	KeyComma	=	0x08,
+	KeyFullStop	=	0x10,
+	KeyAt		=	0x18,
+	KeyAsterisk =	0x2c,
+	KeyMinus	=	0x26,
+	KeyPlus		= 	0x2e,
+
+	KeyShift	=	0x38,	// On a real MO: a yellow triangle.
+	KeyBASIC	=	0x39,
+	KeyControl	=	0x35,
+	KeyRAZ		=	0x33,
+
+	KeySpace	= 	0x20,
+	KeyEnter	=	0x34,
+
+	KeyUp		=	0x31,
+	KeyDown		=	0x21,
+	KeyLeft		=	0x29,
+	KeyRight	=	0x19,
+
+	KeyINS		=	0x09,
+	KeyEFF		=	0x11,
+
+	KeyACC		=	0x36,
+	KeyStop		=	0x37,
 };
 
 struct KeyboardMapper: public MachineTypes::MappedKeyboardMachine::KeyboardMapper {
@@ -21,56 +59,57 @@ struct KeyboardMapper: public MachineTypes::MappedKeyboardMachine::KeyboardMappe
 			default:	return MachineTypes::MappedKeyboardMachine::KeyNotMapped;
 
 			using enum Inputs::Keyboard::Key;
-			case k0:	return 0x1e;		case k1:	return 0x2f;
-			case k2:	return 0x27;		case k3:	return 0x1f;
-			case k4:	return 0x17;		case k5:	return 0x0f;
-			case k6:	return 0x07;		case k7:	return 0x06;
-			case k8:	return 0x0e;		case k9:	return 0x16;
+			case k0:	return Key0;		case k1:	return Key1;
+			case k2:	return Key2;		case k3:	return Key3;
+			case k4:	return Key4;		case k5:	return Key5;
+			case k6:	return Key6;		case k7:	return Key7;
+			case k8:	return Key8;		case k9:	return Key9;
 
-			case Q:		return 0x2d;		case W:		return 0x25;
-			case E:		return 0x1d;		case R:		return 0x15;
-			case T:		return 0x0d;		case Y:		return 0x05;
-			case U:		return 0x04;		case I:		return 0x0c;
-			case O:		return 0x14;		case P:		return 0x1c;
-			case A:		return 0x2b;		case S:		return 0x23;
-			case D:		return 0x1b;		case F:		return 0x13;
-			case G:		return 0x0b;		case H:		return 0x03;
-			case J:		return 0x02;		case K:		return 0x0a;
-			case L:		return 0x12;		case Z:		return 0x30;
-			case X:		return 0x28;		case C:		return 0x32;
-			case V:		return 0x2a;		case B:		return 0x22;
-			case N:		return 0x00;		case M:		return 0x1a;
+			case Q:		return KeyA;		case W:		return KeyZ;
+			case E:		return KeyE;		case R:		return KeyR;
+			case T:		return KeyT;		case Y:		return KeyY;
+			case U:		return KeyU;		case I:		return KeyI;
+			case O:		return KeyO;		case P:		return KeyP;
+			case A:		return KeyQ;		case S:		return KeyS;
+			case D:		return KeyD;		case F:		return KeyF;
+			case G:		return KeyG;		case H:		return KeyH;
+			case J:		return KeyJ;		case K:		return KeyK;
+			case L:		return KeyL;		case Z:		return KeyW;
+			case X:		return KeyX;		case C:		return KeyC;
+			case V:		return KeyV;		case B:		return KeyB;
+			case N:		return KeyN;		case M:		return KeyM;
 
-			case FullStop:	return 0x10;
-			case Comma:		return 0x08;
-			case Hyphen:	return 0x26;
-			case Equals:	return 0x2e;	// +
+			case FullStop:	return KeyFullStop;
+			case Comma:		return KeyComma;
+			case Hyphen:	return KeyMinus;
+			case Equals:	return KeyPlus;
 
-			case OpenSquareBracket:	return 0x18;	// @
-			case Semicolon:			return 0x1a;	// M
+			case OpenSquareBracket:	return KeyAt;
+			case Semicolon:			return KeyM;
 
 			case CloseSquareBracket:
-			case Quote:		return 0x2c;	// Asterisk.
+			case Quote:		return KeyAsterisk;
 
-			case Space:		return 0x20;
-			case Enter:		return 0x34;
-			case Backspace:	return 0x36;
-			case Tab:		return 0x37;
+			case Space:		return KeySpace;
+			case Enter:		return KeyEnter;
+			case Backspace:	return KeyACC;
+			case Escape:	return KeyStop;
 
-			case Up:		return 0x31;	case Down:	return 0x21;
-			case Left:		return 0x29;	case Right:	return 0x19;
+			case Up:		return KeyUp;	case Down:	return KeyDown;
+			case Left:		return KeyLeft;	case Right:	return KeyRight;
 
 			case LeftShift:
-			case RightShift:	return 0x38;
+			case RightShift:	return KeyShift;
+			case Tab:
 			case LeftControl:
-			case RightControl:	return 0x35;
+			case RightControl:	return KeyControl;
 			case LeftOption:
-			case RightOption:	return 0x33;
+			case RightOption:	return KeyBASIC;
 			case LeftMeta:
-			case RightMeta:		return 0x39;
+			case RightMeta:		return KeyRAZ;
 
-			case Insert:		return 0x09;	// INS.
-			case Home:			return 0x11;	// EFF.
+			case Insert:		return KeyINS;
+			case Home:			return KeyEFF;
 		}
 	}
 };
