3107296881434f20ba43822baa6439e36c1cfdd6
Thomas Harte <thomas.harte@gmail.com>
2026-05-01 20:30:15 -0400

    Start making room for the CoCo.

---
+++ Machines/Tandy/CoCo/CoCo.cpp
@@ -0,0 +1,9 @@
+//
+//  CoCo.cpp
+//  Clock Signal
+//
+//  Created by Thomas Harte on 01/05/2026.
+//  Copyright © 2026 Thomas Harte. All rights reserved.
+//
+
+#include "CoCo.hpp"
---
+++ Machines/Tandy/CoCo/CoCo.hpp
@@ -0,0 +1,14 @@
+//
+//  CoCo.hpp
+//  Clock Signal
+//
+//  Created by Thomas Harte on 01/05/2026.
+//  Copyright © 2026 Thomas Harte. All rights reserved.
+//
+
+#ifndef CoCo_hpp
+#define CoCo_hpp
+
+#include <stdio.h>
+
+#endif /* CoCo_hpp */
